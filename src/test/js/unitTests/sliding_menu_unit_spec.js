6c6ec4d472243b28f015e86b03e9847b593e4d60
ignacio-valdivieso <ignacio.valdivieso@olx.com>
2013-02-06 18:55:10 -0300

    [MOB-2536] ARWEN - Menus
    cleaned the unit test for the sliding menu

--- src/test/js/unitTests/sliding_menu_unit_spec.js
+++ src/test/js/unitTests/sliding_menu_unit_spec.js
@@ -1,27 +1,16 @@
 define(['views/home/HomeView'], function(HomeView) {
 describe('The sliding menu',function(){
 	
-	var rootId = 'testContainer';
-	
 	//Create an easily-removed container for our tests to play in
 	beforeEach(function() {
-		var container = document.createElement('div');
-		container.setAttribute('id',rootId);		
-		document.body.appendChild(container);
 
 		setFixtures('<div id="home"></div>');
 		this.homeView = new HomeView();
 
 	});
 	
-	//Clean it up after each spec
-	afterEach(function() {
-		var container = document.getElementById(rootId);
-		container.parentNode.removeChild(container);
-	});
-		
 	//Specs
-	it('opens',function(){
+	it('is attached',function(){
  
       	$(this.homeView.el).attr('data-role', 'page');
       	this.homeView.render();
