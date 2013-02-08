1c62fdbf84d0ee690bdddd85b55d946f6daba230
ignacio-valdivieso <ignacio.valdivieso@olx.com>
2013-02-08 17:52:23 -0300

    [MOB-2552] Populate left menu
    Populated the left menu with the categories fetched from SMAUG

--- src/main/webapp/js/collections/categories.js
+++ src/main/webapp/js/collections/categories.js
@@ -2,11 +2,14 @@ define([
   'underscore',
   'backbone',
   // Pull in the Model module from above
-  'models/category'
-], function(_, Backbone, CategoryModel){
-  var CategoryCollection = Backbone.Collection.extend({
-    model: CategoryModel
-  });
-  // You don't usually return a collection instantiated
-  return CategoryCollection;
+  'models/category',
+  'config/conf'
+], function(_, Backbone, CategoryModel, ConfModel){
+	var conf = new ConfModel();
+  	var CategoryCollection = Backbone.Collection.extend({
+    	model: CategoryModel,
+    	url: conf.get('smaug').url + ':' + conf.get('smaug').port + '/categories/1'
+  	});
+  	// You don't usually return a collection instantiated
+  	return CategoryCollection;
 });
