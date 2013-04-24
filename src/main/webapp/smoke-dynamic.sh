50767e150a64d5df212f5fcbbde8cac45beffb6b
Santiago Villa Fernandez <santiago.villa@olx.com>
2013-04-24 18:08:45 -0300

    Added a new smokes

--- src/main/webapp/smoke-dynamic.sh
+++ src/main/webapp/smoke-dynamic.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -s -H static03.olx-st.com http://192.168.0.101/mobile-arwen/ | grep title | grep -q 'OLX Mobile'
+curl -s http://webapp.olx.com | grep title | grep -q 'OLX Mobile'
--- src/main/webapp/smoke-static.sh
+++ src/main/webapp/smoke-static.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -s -H static03.olx-st.com http://192.168.0.101/mobile-arwen/ | grep title | grep -q 'OLX Mobile'
+curl -s http://static03.olx-st.com/mobile-arwen/js/arwen.js | grep -q 'RequireJS 2.1.5'
