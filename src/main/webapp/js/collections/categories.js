c5fdfc27b9075e0ecce6093f8ecb9968a54c8293
Santiago Villa Fernandez <santiago.villa@olx.com>
2013-05-28 14:14:28 -0300

    [MOB-2967] Fixed new smaug's categories configuration

--- src/main/webapp/js/collections/categories.js
+++ src/main/webapp/js/collections/categories.js
@@ -7,7 +7,7 @@ define([
 ], function(_, Backbone, CategoryModel, Conf){
     var CategoryCollection = Backbone.Collection.extend({
       model: CategoryModel,
-      url: Conf.get('smaug').url + ':' + Conf.get('smaug').port + '/categories/1'
+      url: Conf.get('smaug').url + ':' + Conf.get('smaug').port + '/countries/www.olx.com/categories'
     });
     // You don't usually return a collection instantiated
     return CategoryCollection;
