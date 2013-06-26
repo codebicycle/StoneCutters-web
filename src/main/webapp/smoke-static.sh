eb16e7662243bdc3b14ae70d089a192c32cecc77
Santiago Villa Fernandez <santiago.villa@olx.com>
2013-06-26 18:16:52 -0300

    A new smoke test

--- src/main/webapp/smoke-static.sh
+++ src/main/webapp/smoke-static.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-curl -s http://static03.olx-st.com/mobile-arwen/js/arwen.js | grep -q 'RequireJS 2.1.5'
+CMD="/bin/date +%s"
+URL="http://static03.olx-st.com/mobile-arwen/js/arwen-"
+curl -s $URL`$CMD`.js | grep -q 'RequireJS 2.1.5'
