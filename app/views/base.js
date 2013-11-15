68b74d172daacbb20be77cd89a784ca166f0650c
Santiago Villa Fernandez <santiago.villa@olx.com>
2013-11-15 15:32:09 -0300

    POC of WAP is working.

--- app/views/base.js
+++ app/views/base.js
@@ -2,4 +2,17 @@ var RendrView = require('rendr/shared/base/view');
 
 // Create a base view, for adding common extensions to our
 // application's views.
-module.exports = RendrView.extend({});
+module.exports = RendrView.extend({
+    getTemplate: function(){
+    	var platform = null;
+    	if(global.isServer){
+    		platform = this.app.req.platform;
+    	}else{
+    		//if I am in client, I will fetch the enhanced templates.
+    		platform = "enhanced";
+    	}
+    	var name = this.name;
+    	console.log("retrieving:"+platform+"/"+name);
+        return this.app.templateAdapter.getTemplate(platform+"/"+name);
+    }
+});
