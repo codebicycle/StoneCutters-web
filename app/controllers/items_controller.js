4d3b4fbcd09f67fd348bed6aa4fd744eba1fc063
ignacio-valdivieso <ignacio.valdivieso@olx.com>
2013-12-16 16:41:06 -0300

    search is working

--- app/controllers/items_controller.js
+++ app/controllers/items_controller.js
@@ -8,7 +8,6 @@ module.exports = {
     //don't read from cache, because rendr caching expects an array response
     //with ids, and smaug returns an object with 'data' and 'metadata'
     this.app.fetch(spec, {"readFromCache": false}, function(err, result) {
-      console.log(result.items.models[0].get("data")[0].title);
       result.items = result.items.models[0].get("data");
       callback(err, result);
     });
@@ -21,5 +20,23 @@ module.exports = {
     this.app.fetch(spec, function(err, result) {
       callback(err, result);
     });
-  }
+  },
+
+  search: function(params, callback) {
+    params.item_type = 'adsList';
+    params.searchTerm = params.q;
+    delete params.q;
+
+    var spec = {
+      items: {collection: 'Items', params: params}
+    };
+    //don't read from cache, because rendr caching expects an array response
+    //with ids, and smaug returns an object with 'data' and 'metadata'
+    this.app.fetch(spec, {"readFromCache": false}, function(err, result) {
+      result.metadata = result.items.models[0].get("metadata");
+      result.items = result.items.models[0].get("data");
+      result.searchTerm = params.searchTerm;
+      callback(err, result);
+    });
+  },
 };
--- app/views/app_view.js
+++ app/views/app_view.js
@@ -4,15 +4,20 @@ var $body = $('body');
 
 module.exports = BaseAppView.extend({
 	className: 'app_view_index_view',
-	
+
 	events:{
 	   'click #toggle-panel': 'toggleLeftPanel',
+	   'click #toggle-search': 'toggleSearch',
     },
 
   	postInitialize: function() {
     	this.app.on('change:loading', function(app, loading) {
     	  $body.toggleClass('loading', loading);
     	}, this);
+
+    	jQuery('#search-bar').change(function(){
+          window.location = "search?location="+"www.olx.com.ar"+"&q=" + jQuery('#search-bar').val();
+        });
   	},
 
   	toggleLeftPanel: function(e){
@@ -21,6 +26,22 @@ module.exports = BaseAppView.extend({
         
         $('body').toggleClass('left-panel-visible');
     },
+
+    toggleSearch: function(e){
+	    if (e) {
+	    	e.preventDefault();
+	    }
+
+	    $("#search-container").toggle();
+
+	    if ($("#search-container").is(":visible")){
+	      $('#search-bar').focus();
+	      //$('#toggle-search .ui-btn-text').text('Cancel');
+	    }else{
+	      $('#search-bar').val("");
+	      //$('#toggle-search .ui-btn-text').text('Search');
+	    }
+	},
 });
 
 module.exports.id = 'app_view/index';
