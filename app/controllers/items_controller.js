7bb67fa8e0a9fa3593b14903d6ea73c96b76631f
ignacio-valdivieso <ignacio.valdivieso@olx.com>
2013-12-16 15:30:39 -0300

    fixed a caching bug with rendr

--- app/controllers/items_controller.js
+++ app/controllers/items_controller.js
@@ -3,9 +3,13 @@ module.exports = {
     params.item_type = 'adsList';
     
     var spec = {
-      collection: {collection: 'Items', params: params}
+      items: {collection: 'Items', params: params}
     };
-    this.app.fetch(spec, function(err, result) {
+    //don't read from cache, because rendr caching expects an array response
+    //with ids, and smaug returns an object with 'data' and 'metadata'
+    this.app.fetch(spec, {"readFromCache": false}, function(err, result) {
+      console.log(result.items.models[0].get("data")[0].title);
+      result.items = result.items.models[0].get("data");
       callback(err, result);
     });
   },
