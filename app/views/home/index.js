430d5fb19efdaa9ee9c312c36afcf3c5d0fa56b3
Mauricio Scardaccione <mauricio.scardaccione@olx.com>
2014-02-11 17:50:49 -0300

    [MOB-4184] ARWEN :: Homepage HTML5

--- app/views/home/index.js
+++ app/views/home/index.js
@@ -1,6 +1,7 @@
 var BaseView = require('../base');
 var _ = require('underscore');
 var fitText = require('../../helpers/fitText_helper');
+var timeAgo = require('../../helpers/timeAgo_helper');
 
 if (typeof window != 'undefined') {
 	var Swipe = require('../../lib/swiper');
@@ -9,10 +10,17 @@ if (typeof window != 'undefined') {
 module.exports = BaseView.extend({
     className: 'home_index_view',
 
+    processItem: function(item){
+        var dateAg = timeAgo.timeAgo(new Date(item.date.year, item.date.month - 1, item.date.day, item.date.hour, item.date.minute, item.date.second, 00));
+        item.date.since = dateAg;
+    },
+
     getTemplateData:function(){
         // Get `super`.
         var data = BaseView.prototype.getTemplateData.call(this);
         
+        _.each(data.whatsNewItems,this.processItem);
+
         return _.extend({}, 
                         data, 
                         {
@@ -23,7 +31,7 @@ module.exports = BaseView.extend({
     postRender: function(){
 
         fitText.fitText($('section#newAds .swiper-containerAds .caption') , .9 , { minFontSize: '9px', maxFontSize: '30px' });
-        fitText.fitText($('section#categories .swiper-containerCats .slide div p') , .7 , { minFontSize: '9px', maxFontSize: '30px' });       
+        fitText.fitText($('section#categories .swiper-containerCats .slide div p') , .7 , { minFontSize: '9px', maxFontSize: '30px' });    
 
         var swiperAds = $('.swiper-containerAds').swiper({
             mode:'horizontal',
