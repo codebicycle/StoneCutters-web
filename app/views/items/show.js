9eb59b59ee9fd7bf847829240b2c69b11d872ddd
Mauricio Scardaccione <mauricio.scardaccione@olx.com>
2014-02-13 12:30:27 -0300

    [MOB-4309] ARWEN :: Buy Flow HTML5

--- app/views/items/show.js
+++ app/views/items/show.js
@@ -8,20 +8,46 @@ if (typeof window != 'undefined') {
 module.exports = BaseView.extend({
   className: 'items_show_view',
 
-  getTemplateData: function() {
-    var data = BaseView.prototype.getTemplateData.call(this);
-    data.category_name = this.options.category_name;
-    return data;
-  },
+	paginationSize: function(){
+		var paginationCount = $('.slidePagination span').length + 1;
+		var windowSize = $(window).width();
+		var paginationWidth = windowSize / paginationCount;
+		var paginationMargin = paginationWidth / paginationCount;
+		paginationWidth = paginationWidth - paginationMargin; 
 
-  postRender: function(){
-  	var mySwiper = $('.swiper-containerItem').swiper({
-		mode:'horizontal',
-		loop: true,
-		pagination: '.slidePagination',
-		paginationClickable: true,
-		initialSlide: 0
-	});
-  },
+		$('.slidePagination span').css('width' , paginationWidth+'px');
+		$('.slidePagination span').css('margin' , '0 '+paginationMargin+'px');
+	},
+
+
+  	getTemplateData: function() {
+    	var data = BaseView.prototype.getTemplateData.call(this);
+    	data.category_name = this.options.category_name;
+    	data.item.location.cityName = data.item.location.children[0].children[0].name; 
+    	console.log(data.item.location.cityName);
+    	return data;
+  	},
+
+  	postRender: function(){
+  		
+  		var mySwiper = $('.swiper-containerItem').swiper({
+			mode:'horizontal',
+			loop: true,
+			pagination: '.slidePagination',
+			paginationClickable: true,
+			initialSlide: 0
+		});
+		this.paginationSize();
+
+		$('section#itemPage').css('margin-bottom' , ($('#actions').height()+20)+'px');
+
+		$( window ).resize(function() {
+		  $('section#itemPage').css('margin-bottom' , ($('#actions').height()+20)+'px');
+		  paginationSize();
+		});
+	}
+  
+
+
 });
 module.exports.id = 'items/show';
