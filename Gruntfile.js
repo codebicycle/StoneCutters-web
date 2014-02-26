b2ebbea4a2264aa80c6405c3dddfa3ef2cb67d09
Santiago Villa Fernandez <santiago.villa@olx.com>
2014-02-26 15:57:32 -0300

    Added steps in order to validate anonymous functions.

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,6 +40,12 @@ module.exports = function(grunt) {
         privateKey: grunt.file.read(grunt.file.readJSON('conf.json').auth) //"/home/dev/.ssh/id_rsa"
       }
     },
+    exec: {
+      add_new_line: 'sed -e \'$a\\\' build/build_traceur.js > build/build.js',
+      anonymous_functions: '! grep -e "[^:= ][ ]*function()" app/collections/** app/controllers/** app/helpers/** app/models/** app/templates/** app/views/**/** server/**/** app/**',
+      delete_compiled_templates: 'rm -rf app/templates/compiledTemplates.js',
+      delete_merged_assets: 'rm -rf public/mergedAssets.js'
+    },
     sshexec: {
       start:{
         command: "cd /root/apps/arwen/ && start arwen",
@@ -54,14 +60,27 @@ module.exports = function(grunt) {
           ignoreErrors: true
         }
       },
-       'npm-install':{
+      'npm-install':{
         command: "cd /root/apps/arwen/ && npm install --verbose",
         options: {
           config: 'testing', 
           ignoreErrors: true
         }
        }
-    },  
+    },
+
+    //Traceur i.e. ES6 transpiler
+    traceur: {
+      options: {
+        experimental:true,
+        blockBinding:true
+      },
+      custom: {
+        files:{
+          'build/mergedAssets.js': ['public/mergedAssets.js']
+        } 
+      },
+    }, 
 
     //Stylus
     stylus: {
@@ -95,6 +114,21 @@ module.exports = function(grunt) {
           // i.e. __layout.hbs.
           return filename.slice(0, 2) !== '__';
         }
+      }
+    },
+
+    // make a zipfile
+    compress: {
+      main: {
+        options: {
+          archive: 'archive.zip'
+        },
+        files: [
+          {src: ['public/*'], dest:  'target/static/'}, // includes files in path
+          {src: ['path/**'], dest: 'target/static/'}, // includes files in path and its subdirs
+          {expand: true, cwd: 'path/', src: ['**'], dest: 'internal_folder3/'}, // makes all src relative to cwd
+          {flatten: true, src: ['path/**'], dest: 'internal_folder4/', filter: 'isFile'} // flattens results to a single level
+        ]
       }
     },
 
@@ -147,6 +181,7 @@ module.exports = function(grunt) {
           dest: 'public/mergedAssets.js',
           src: [
             'app/**/*.js',
+            //'build/all.js',
             rendrDir + '/client/**/*.js',
             rendrDir + '/shared/**/*.js',
             rendrHandlebarsDir + '/index.js',
@@ -229,12 +264,25 @@ module.exports = function(grunt) {
     });
   });
 
+  //clean tasks
+  grunt.registerTask('clean', ['exec:delete_compiled_templates', 'exec:delete_merged_assets']);
+
+  //Transpiler task
+  grunt.registerTask('es6', ['traceur', 'exec:add_new_line']);
+  
   //Testing task
   grunt.registerTask('unit-test', ['jshint', 'mochaTest']);
+
+  
+  //Search anonymous functions
+  grunt.registerTask('anonymous-finder', ['exec:anonymous_functions', 'anonymous-fx']);
+  grunt.registerTask('anonymous-fx', 'Search for anonymous functions.', function handler(){
+    grunt.task.requires('exec:anonymous_functions');
+  });
   
   //Compile tasks (dev-build, dist-build)
   grunt.registerTask('dev-build',  ['handlebars', 'rendr_stitch', 'stylus']);
-  grunt.registerTask('dist-build', ['handlebars', 'rendr_stitch', 'stylus', 'uglify']);
+  grunt.registerTask('dist-build', ['clean','anonymous-finder','handlebars', 'rendr_stitch', 'stylus', 'uglify']);
 
   //Pipeline tasks
   //Here we compile and deploy the sourcecode.
@@ -242,8 +290,8 @@ module.exports = function(grunt) {
 
   //Server tasks
   // Run the server and watch for file changes
-  grunt.registerTask('server-dev', ['runNode', 'dev-build', 'watch']);
+  grunt.registerTask('server-dev', ['clean','anonymous-finder','runNode', 'dev-build', 'watch']);
   
-    // Default task(s).
+  // Default task(s).
   //grunt.registerTask('default', ['compile']);
 };
