63a10555e13e0241e0993612ff82090a70556ade
Santiago Villa Fernandez <santiago.villa@olx.com>
2014-03-05 16:03:00 -0300

    MOB-[4542] Added the firsat cluster approach

---
+++ cluster.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/*
+ * This is the implementation of cluster technology for ARWEN.
+ */
+ module.exports = function(done) {
+    var cluster = require('cluster');
+
+    if (cluster.isMaster) {
+        var cpuCount = require('os').cpus().length;
+        var i;
+
+        for (i = 0; i < cpuCount; i++) {
+            console.log("I am forking a new process");
+            cluster.fork();
+        }
+        cluster.on('exit', function onClusterExit(worker) {
+            console.log('Express server %d exiting', worker.id);
+            //You won't die.
+            cluster.fork();
+        });
+    }
+    else {
+        done(cluster.worker);
+    }
+}
