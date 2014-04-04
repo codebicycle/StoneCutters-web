9700f292f6c3242c720f8ceaf4d7cdde8a6d6e28
Nicolas Molina <nicolas.molina@olx.com>
2014-04-04 10:47:39 -0300

    [MOB-4708] Pagination on listings

--- app/controllers/items_controller.js
+++ app/controllers/items_controller.js
@@ -1,6 +1,9 @@
 'use strict';
 
 var helpers = require('../helpers');
+var _ = require('underscore');
+var querystring = require('querystring');
+//var CONFIG = require('config');
 
 module.exports = {
     index: function(params, callback) {
@@ -11,6 +14,13 @@ module.exports = {
                 params: params
             }
         };
+        var query = _.clone(params);
+        function checkPageSize(query) {
+            if (query.pageSize > 50 /*CONFIG.maxPageSize*/) {
+                query.pageSize = 50 /*CONFIG.maxPageSize*/;
+            }
+        }
+        checkPageSize(params);
 
         params.item_type = 'adsList';
 
@@ -19,7 +29,42 @@ module.exports = {
         app.fetch(spec, {
             'readFromCache': false
         }, function afterFetch(err, result) {
-            result.items = result.items.models[0].get('data');
+            function prepareNextLink(metadata, url) {
+                var next = metadata.next;
+                if (next) {
+                    query.offset = next.replace(/.*offset=(\d*).*/, '\$1');
+                    query.pageSize = next.replace(/.*pageSize=(\d*).*/, '\$1');
+                    query.sort = next.replace(/.*sort=([\d\w\s]*).*/, '\$1');
+                    metadata.next = (url + querystring.stringify(query));
+                } else {
+                    query.offset = parseInt(query.offset) + parseInt(query.pageSize);
+                }
+                checkPageSize(query);
+            }
+
+            function preparePreviousLink(metadata, url) {
+                var offset = parseInt(query.offset);
+                var pageSize = parseInt(query.pageSize);
+                offset = (offset - (pageSize * 2));
+                if (offset >= 0) {
+                    query.offset = offset;
+                    metadata.previous = (url + querystring.stringify(query));
+                }
+            }
+
+            function preparePaginationLink(metadata) {
+                var url;
+                if (metadata.total > 0) {
+                    url = '/items?';
+                    prepareNextLink(metadata, url);
+                    preparePreviousLink(metadata, url);
+                }
+             }
+
+            var model = result.items.models[0];
+            result.items = model.get('data');
+            result.metadata = model.get('metadata');
+            preparePaginationLink(result.metadata);
             result.platform = app.getSession('platform');
             callback(err, result);
         });
