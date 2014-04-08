e38e728098c058ce1e5d1f63b19b3e7635455203
Gonzalo Aizpun <gonzalo.aizpun@olx.com>
2014-04-08 17:00:38 -0300

    [MOB-4659] build.json

--- deployment/artifactory.sh
+++ deployment/artifactory.sh
@@ -5,6 +5,7 @@ npm install
 
 # write config
 echo '{"auth":"/var/lib/jenkins/.ssh/id_rsa"}' > conf.json
+echo '{"build_version":'${BUILD_NUMBER}',"static_version":"[STATIC_VERSION]"}' > build.json
 
 # execute grunt job
 grunt pipeline --artifactory-version=${BUILD_NUMBER}
