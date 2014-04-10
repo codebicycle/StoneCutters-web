0f07a4cb7858d5e6855c8adc325128c75ffa9898
Gonzalo Aizpun <gonzalo.aizpun@olx.com>
2014-04-10 16:13:17 -0300

    [MOB-4837] /status resource

--- server/router/pages.js
+++ server/router/pages.js
@@ -7,6 +7,7 @@ var configClient = require('../../app/config');
 module.exports = function itemRouter(app, dataAdapter) {
     app.get('/health', healthHandler);
     app.get('/check', checkHandler);
+    app.get('/stats', statsHandler);
 
     function healthHandler(req, res) {
         res.json({
@@ -16,11 +17,25 @@ module.exports = function itemRouter(app, dataAdapter) {
     }
 
     function checkHandler(req, res) {
-
         res.json({
             server: configServer.get(),
             client: configClient.get()
         });
+    }
+
+    function statsHandler(req, res) {
+        var list = [];
+
+        list.push('environment.platform:' + process.platform);
+        list.push('environment.architecture:' + process.arch);
+        list.push('process.pid:' + process.pid);
+        list.push('process.title:' + process.title);
+        list.push('node.version:' + process.versions.node);
+        list.push('node.uptime:' + process.uptime());
+        list.push('memory.rss:' + process.memoryUsage().rss);
+        list.push('memory.heapTotal:' + process.memoryUsage().heapTotal);
+        list.push('memory.heapUsed:' + process.memoryUsage().heapUsed);
+        res.send(list.join(' '));
     }
 
 };
