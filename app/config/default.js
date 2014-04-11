bf5ff6586ad19f8a593262951cb9e5382d2e868a
Nicolas Molina <nicolas.molina@olx.com>
2014-04-11 00:36:03 -0300

    /force

--- app/config/default.js
+++ app/config/default.js
@@ -2,12 +2,13 @@ module.exports = {
     smaug: {
         maxPageSize: 50
     },
+    platforms: ['wap', 'html4', 'html5', 'desktop'],
     staticAccept: ['css', 'js'],
     imageAccept: ['jpg', 'jpeg', 'png', 'gif', 'ico'],
     environment: {
         type: 'd',
         staticPath: '',
         imagePath: ''
-    }
+    },
 };
-    
+
