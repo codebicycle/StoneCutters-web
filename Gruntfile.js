9fc31c8a6ed33fdbe1a5a49cf4eeb0caf1ac6ad4
Martin Di Domenico <martin.didomenico@olx.com>
2014-04-23 18:11:44 -0300

    Pipetest
    grunt task for testing the pipeline

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,8 +17,20 @@ module.exports = function(grunt) {
 
     grunt.registerTask('log', function log() {
         grunt.util.spawn({
-            cmd: 'npm',
+            cmd: 'node',
             args: ['run-script', 'debug'],
+            opts: {
+                stdio: 'inherit'
+            }
+        }, function error(err) {
+            grunt.fail.fatal(err.stack);
+        });
+    });
+
+    grunt.registerTask('startDist', function log() {
+        grunt.util.spawn({
+            cmd: './dist/start.sh',
+            args: [grunt.option('environment') || 'testing'],
             opts: {
                 stdio: 'inherit'
             }
@@ -35,9 +47,13 @@ module.exports = function(grunt) {
 
     grunt.registerTask('jshint:node', ['jshint:server', 'jshint:client']);
 
+    grunt.registerTask('copy:code', ['copy:dynamic', 'copy:static']);
+
     grunt.registerTask('start', ['rebuild', 'jshint:node', 'develop', 'watch']);
 
     grunt.registerTask('debug', ['rebuild', 'jshint:node', 'log', 'watch']);
+
+    grunt.registerTask('pipetest', ['exec:removeDist', 'rebuild', 'copy:code', 'gitclone', 'copy:config', 'exec:removeDistGit', 'exec:chmodDistStart', 'startDist', 'watch:dist']);
 
     grunt.registerTask('pipeline', ['rebuild', 'artifactory:static:publish', 'artifactory:dynamic:publish']);
 
