4bbf59286e7103ba0f200bb039b69149527c64d2
Gonzalo Aizpun <gonzalo.aizpun@olx.com>
2014-04-24 14:10:47 -0300

    [MOB-4659] size improvements

--- grunt/copy.js
+++ grunt/copy.js
@@ -1,10 +1,16 @@
 'use strict';
 
 module.exports = function(grunt) {
+    var files = ['app/**/*', '!app/config/default.js', 'server/**/*', 'index.js', 'build.json', 'newrelic.js', 'package.json'];
+    var dependencies = grunt.file.readJSON('package.json').dependencies;
+
+    for(var module in dependencies) {
+        files.push('node_modules/' + module + '/**/*');
+    }
     return {
         dynamic: {
             files: [{
-                src: ['app/**/*', '!app/config/default.js', 'node_modules/**/*', 'server/**/*', 'index.js', 'build.json', 'newrelic.js', 'start.sh', 'package.json'],
+                src: files,
                 dest: 'dist/'
             }]
         },
