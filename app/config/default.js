3e677866bb4f8e1fcaaa0dc92960f7b678da234e
Martin Di Domenico <martin.didomenico@olx.com>
2014-05-05 10:29:46 -0300

    [MOB-4659] Pipeline
    Config change

--- app/config/default.js
+++ app/config/default.js
@@ -2,7 +2,6 @@ module.exports = {
     smaug: {
         maxPageSize: 10
     },
-    platforms: ['wap', 'html4', 'html5', 'desktop'],
     staticAccept: ['css', 'js'],
     imageAccept: ['jpg', 'jpeg', 'png', 'gif', 'ico']
 };
