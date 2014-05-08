e133101f33ea97085ed287806c079f8642739a4b
Mauricio Scardaccione <mauricio.scardaccione@olx.com>
2014-05-08 11:36:48 -0300

    Reply to ad html5

--- app/views/items/show.js
+++ app/views/items/show.js
@@ -111,8 +111,53 @@ module.exports = BaseView.extend({
             e.preventDefault();
             $('body').removeClass('noscroll');
             $(this).parents('.popup').removeClass('visible');
-        }); 
+        });
+        var self = this;
+        $('#replyForm .submit').click(function() {
+            var message = $('.message').val();
+            var email = $('.email').val();
+            var name = $('.name').val();
+            var phone = $('.phone').val();
+            var itemId = $('.itemId').val();
+            var data = {message: message,email: email, name:name,phone:phone};
+            if(self.validForm(message, name, email)){
+                $('.loading').show();
+                $('body').addClass('noscroll');
+                var url = '/items/'+itemId+'/reply';
+                $.ajax({
+                    type: "POST",
+                    url: url,
+                    cache: false,
+                    data: data,
+                    success: function(data) {
+                        console.log(data.error);
+                        $('.loading').hide();
+                        $('body').removeClass('noscroll');
+                        $('.message').val('');
+                        $('.name').val('');
+                        $('.email').val('');
+                        $('.phone').val('');
+                        $('.msgCont .msgCont-wrapper .msgCont-container').text('Se envio');
+                        $('.msgCont').addClass('visible');
+                        setTimeout(function(){
+                            $('.msgCont').removeClass('visible');
+                       }, 3000);
+                    },
+                    error: function() {
+                        $('small.email').text('error mail smaug').removeClass('hide');
+                    }
+                });
+            }
+        
+        });
+        $('form#replyForm').on('change', 'input.name , input.email , textarea.message', function (e) {
+            var value = $(this).val();
+            var field = $(this).attr('class');
 
+            if(self.isEmpty(value,field) && field == 'email'){
+                self.isEmail(value,field);
+            }
+        });
     },
     remove: function() {
         $(window).off('resize', this.resize);
@@ -127,7 +172,45 @@ module.exports = BaseView.extend({
         paginationWidth = paginationWidth - paginationMargin;
         $('.slidePagination span').css('width' , paginationWidth+'px');
         $('.slidePagination span').css('margin' , '0 '+paginationMargin+'px');
+    },
+    validForm: function (message, name, email) {           
+        var valMail = true;
+        var valName = true;
+        var valMsg = true;
+
+        valMail = this.isEmpty(email,'email');
+        if(valMail){
+            valMail = this.isEmail(email,'email');
+        }
+        
+        valName = this.isEmpty(name,'name');
+        valMsg = this.isEmpty(message,'message');
+
+        return (valMail && valName && valMsg);
+    },
+    isEmail: function (value,field) {
+            var expression = /^[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,6})$/;
+            if(!expression.test(value)){
+                $('small.'+field).text('no mail').removeClass('hide');
+                return false;
+            }else{
+                $('small.'+field).removeClass('hide').empty();
+                return true;
+            }
+            
+        },
+
+    isEmpty: function (value,field) {
+
+            if(value === ''){
+                $('small.'+field).text('mandatory').removeClass('hide');
+                return false;
+            }else{
+                $('small.'+field).addClass('hide').empty();
+                return true;
+            }
     }
+
 });
 
 module.exports.id = 'items/show';
