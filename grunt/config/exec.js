af6a0e48c32e7f8f327598ec5aa1afdc6972539e
Martin Di Domenico <martin.didomenico@olx.com>
2014-05-14 16:58:11 -0300

    Minor translations improvement
    Correct tmp folder

--- grunt/config/exec.js
+++ grunt/config/exec.js
@@ -3,7 +3,7 @@
 module.exports = function(grunt) {
     return {
         anonymousFunctions: '! grep -e "[^:= ][ ]*function()" app/collections/** app/controllers/** app/helpers/** app/models/** app/templates/** app/views/**/** server/**/** app/**',
-        removeTranslations: 'rm -rf translations app/translations public/js/app/translations.js public/js/min/translations.*',
+        removeTranslations: 'rm -rf translations-tmp app/translations public/js/app/translations.js public/js/min/translations.*',
         removeTemplates: 'rm -rf app/templates/compiled',
         removeAssets: 'rm -rf public/js/app public/js/min',
         removeStyles: 'rm -rf public/css',
