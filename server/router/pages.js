df6447f7912a98d1ec53080a0acfeb6d427b2284
Nicolas Molina <nicolas.molina@olx.com>
2014-05-14 15:20:55 -0300

    [MOB-5032] Tracking

--- server/router/pages.js
+++ server/router/pages.js
@@ -125,7 +125,7 @@ module.exports = function itemRouter(app, dataAdapter) {
 
             if (atiConfig) {
                 analytic = new Analytic('ati', {
-                    id: 533932, // atiConfig.siteId,
+                    id: atiConfig.siteId,
                     host: atiConfig.logServer,
                     clientId: req.rendrApp.getSession('clientId').substr(24)
                 });
