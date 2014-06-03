0926ba3f4f369f70d1f9feb86b941eb121f12fc6
Andres Risso <andresr@olx.com>
2014-06-03 16:02:18 -0300

    [MOB-5422] ARWEN :: GENERAL :: Error pages

--- app/views/pages/error.js
+++ app/views/pages/error.js
@@ -1,11 +1,19 @@
 'use strict';
 
 var BaseView = require('../base');
+var _ = require('underscore');
 
 module.exports = BaseView.extend({
     className: 'pages_error_view',
     wapAttributes: {
         cellpadding: 0
+    },
+    getTemplateData: function() {
+        var data = BaseView.prototype.getTemplateData.call(this);
+
+        return _.extend({}, data, {
+            blackBar: this.app.getSession('blackBar')
+        });
     }
 });
 
