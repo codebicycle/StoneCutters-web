5361f1e698ca2888b20f41c88ca58672540605a5
Andres Risso <andresr@olx.com>
2014-07-21 12:17:48 -0300

    [MOB-5817] ARWEN :: Implement Fuleco

--- app/localized/default/app/views/desktop/footer/footer.js
+++ app/localized/default/app/views/desktop/footer/footer.js
@@ -5,5 +5,18 @@ var Base = require('../../../../../common/app/bases/view').requireView('footer/f
 module.exports = Base.extend({
     tagName: 'footer',
     id: 'footer',
-    className: 'footer_view'
+    className: 'footer_view',
+    events: {
+        'click ul li a:not([href])': 'toggleTeaser',
+        'click .teaser header a': 'closeTeaser'
+    },
+    toggleTeaser: function(e) {
+        var $teaser = $('.teaser', this.$el);
+        $teaser.toggleClass('open');
+    },
+    closeTeaser: function(e) {
+        var $teaser = $('.teaser', this.$el);
+        $teaser.removeClass('open');
+    }
+
 });
