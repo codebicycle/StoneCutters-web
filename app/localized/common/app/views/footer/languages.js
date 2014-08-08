102d9678e211ee5965207df242f4d22d0c589205
Mauricio Scardaccione <mauricio.scardaccione@olx.com>
2014-08-08 15:08:52 -0300

    [MOB-6214] ARWEN :: GENERAL :: After changing language user is always redirected to home page

--- app/localized/common/app/views/footer/languages.js
+++ app/localized/common/app/views/footer/languages.js
@@ -16,31 +16,7 @@ module.exports = Base.extend({
             languages: this.app.session.get('languages'),
             selectedLanguage: this.app.session.get('selectedLanguage')
         });
-    },
-    postRender: function() {
-        $('body').on('change:location', this.changeLocation.bind(this));
-    },
-    changeLocation: function (e, siteLocation) {
-        this.$('.footer-links .footer-link').each(function(i, link) {
-            var $link = $(link);
-            var href = $link.attr('href');
-            var currentLocation = utils.params(href, 'location');
-
-            if (currentLocation !== siteLocation) {
-                if (siteLocation && ~siteLocation.indexOf('www')) {
-                    href = utils.removeParams(href, 'location');
-                }
-                else {
-                    href = utils.link(href, this.app, {
-                        location: siteLocation
-                    });
-                }
-                $link.attr({
-                    href: href
-                });
-            }
-        }.bind(this));
-    }
+    }    
 });
 
 module.exports.id = 'footer/languages';
