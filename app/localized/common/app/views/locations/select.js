e8863f9e3b739b81581da9e1820cbba75b5d04a5
Andres Risso <andresr@olx.com>
2014-08-12 16:21:16 -0300

    [MOB-5956] ARWEN :: WAP :: Missing H1 and H2 tags
    Fixed location tags

--- app/localized/common/app/views/locations/select.js
+++ app/localized/common/app/views/locations/select.js
@@ -10,7 +10,7 @@ module.exports = Base.extend({
     },
     getTemplateData: function() {
         var data = Base.prototype.getTemplateData.call(this);
-        var heading = (data.url !== '/') ? 3 : 1;
+        var heading = data.url === '/';
 
         return _.extend({}, data, {
             heading: heading
