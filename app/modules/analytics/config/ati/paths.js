5ec1cc65c802a98094d0358f277a93e14bddf29a
Martin Di Domenico <martin.didomenico@olx.com>
2014-08-22 12:43:56 -0300

    [MOB-6078] ARWEN :: Tracking verification
    Trackers per country

--- app/modules/analytics/config/ati/paths.js
+++ app/modules/analytics/config/ati/paths.js
@@ -1,3 +1,5 @@
+'use strict';
+
 module.exports = {
     //Development+
     0 : {
---
+++ app/modules/analytics/config/google/trackers.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+    'default': 'UA-5247560-2',
+    'development': 'UA-50756825-1',
+    'staging': 'UA-50756825-1',
+    'testing': 'UA-50756825-1'
+};
