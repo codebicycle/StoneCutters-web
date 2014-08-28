b2ca1b33da9d48ff6ea0c9902334a0d58b47dcc2
mauricio-scardaccione <mauricio.scardaccione@olx.com>
2014-08-28 17:27:07 -0300

    Home Desktop

--- app/localized/default/app/views/desktop/footer/footer.js
+++ app/localized/default/app/views/desktop/footer/footer.js
@@ -8,7 +8,24 @@ module.exports = Base.extend({
     className: 'footer_view',
     
     postRender: function() {
-        
+        $('#footer ul li span').click(function() {
+            var current = $(this).attr('class');
+            if($('#footer').find('.teaser').hasClass('open') && !$('.teaser#' + current).hasClass('open')) {
+                $('.teaser.open').slideToggle( "slow", function() {
+                    $('.teaser.open').toggleClass('open');
+                    $('.teaser#' + current).slideToggle( "slow", function() {
+                        $('.teaser#' + current).toggleClass('open');      
+                    });   
+                });
+            } else {
+                $('.teaser#' + current).slideToggle( "slow", function() {
+                    $('.teaser#' + current).toggleClass('open');      
+                });
+            }
+        });
+        $('#footer .teaser header span').click(function() {
+            $('.teaser.open').slideToggle('slow');
+        });
     }
 
 });
