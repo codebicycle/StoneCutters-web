e45325dc26e39e5f5f36142ad34c3b41ffd4310b
Andres Risso <andresr@olx.com>
2014-09-02 17:23:05 -0300

    [MOB-6445] ARWEN :: PWMPN :: Add customized labels for .co.za

--- app/config/default-development.js
+++ app/config/default-development.js
@@ -19,7 +19,7 @@ module.exports = {
         type: 'development'
     },
     localization: {
-        wap: ['www.olx.de', 'www.olx.fr', 'www.olx.es'],
+        wap: ['www.olx.de', 'www.olx.fr', 'www.olx.es', 'www.olx.co.za'],
         html4: ['www.olx.com.br', 'www.olx.de', 'www.olx.fr', 'www.olx.es', 'www.olx.in', 'www.olx.co.za'],
         html5: ['www.olx.com.br', 'www.olx.de', 'www.olx.fr', 'www.olx.es', 'www.olx.in', 'www.olx.co.za'],
         desktop: ['www.olx.com.br']
--- app/config/default-staging.js
+++ app/config/default-staging.js
@@ -21,7 +21,7 @@ module.exports = {
         imagePath: 'http://images-staging.olx-st.com/mobile-webapp'
     },
     localization: {
-        wap: ['www.olx.de', 'www.olx.fr', 'www.olx.es'],
+        wap: ['www.olx.de', 'www.olx.fr', 'www.olx.es', 'www.olx.co.za'],
         html4: ['www.olx.com.br', 'www.olx.de', 'www.olx.fr', 'www.olx.es', 'www.olx.in', 'www.olx.co.za'],
         html5: ['www.olx.com.br', 'www.olx.de', 'www.olx.fr', 'www.olx.es', 'www.olx.in', 'www.olx.co.za'],
         desktop: []
