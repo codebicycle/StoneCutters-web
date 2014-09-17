81e5011197fec52be68b380bdc8015d50de63fe9
Mauricio Scardaccione <mauricio.scardaccione@olx.com>
2014-09-17 16:54:01 -0300

    Super merge

--- app/modules/analytics/tracker.js
+++ app/modules/analytics/tracker.js
@@ -5,16 +5,12 @@ var configAnalytics = require('./config');
 var google = require('./google');
 var ati = require('./ati');
 var utils = require('../../../shared/utils');
-var esi = require('../esi');
 
 function stringifyParams(params) {
     var str = [];
 
     _.each(params, function(value, name) {
-        if (!esi.isEsiString(value)) {
-            value = encodeURIComponent(value);
-        }
-        str.push(name + '=' + value);
+        str.push(name + '=' + encodeURIComponent(value));
     });
     return str.join('&');
 }
@@ -55,10 +51,10 @@ function generate(query) {
         sid = this.app.session.get('sid');
 
         if (sid) {
-            params.sid = esi.esify.call(this, '$(sid)', sid);
+            params.sid = sid;
         }
-        params.r = esi.esify.call(this, '$rand()', Math.round(Math.random() * 1000000));
-        params.referer = esi.esify.call(this, '$url_encode($(HTTP_REFERER|\'-\'))', (this.app.session.get('referer') || '-'));
+        params.r = Math.round(Math.random() * 1000000);
+        params.referer = (this.app.session.get('referer') || '-');
         params.locNm = location.name;
         params.locId = location.id;
         params.locUrl = location.url;
