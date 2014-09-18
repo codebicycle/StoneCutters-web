21bc842a1c1dffa7d24fe9b8c8ab1d8b91c6c111
Andres Risso <andresr@olx.com>
2014-09-18 14:33:42 -0300

    [MOB-5055] ARWEN :: NPF

--- app/localized/default/app/views/html5/post/flow/header.js
+++ app/localized/default/app/views/html5/post/flow/header.js
@@ -19,7 +19,7 @@ module.exports = Base.extend({
         'restart': 'onRestart'
     },
     onLogoClick: function(event) {
-        if (!confirm('Are you sure you want to leave this page?')) { // Translate this!!!!!
+        if (!confirm(this.parentView.dictionary['misc.WantToGoBack'])) { // Now find a better translation
             event.preventDefault();
             event.stopPropagation();
             event.stopImmediatePropagation();
