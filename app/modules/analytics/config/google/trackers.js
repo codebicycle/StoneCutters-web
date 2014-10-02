566ee43d7e59f6d0cca3eb445328e7c2b291d309
Guillermo Maquieira <guillermom@olx.com>
2014-10-02 11:39:57 -0300

    Tracking :: GA :: Merge

--- app/modules/analytics/config/google/trackers.js
+++ app/modules/analytics/config/google/trackers.js
@@ -1,44 +1,164 @@
 'use strict';
 
 module.exports = {
-    'default': 'UA-54083301-44',
-    'development': 'UA-50756825-1',
-    'staging': 'UA-50756825-1',
-    'testing': 'UA-50756825-1',
-    'www.olx.com.br': 'UA-54083301-20',
-    'www.olx.in': 'UA-54083301-2',
-    'www.olx.com.ar': 'UA-54083301-22',
-    'www.olx.com.pk': 'UA-54083301-6',
-    'www.olx.co.za': 'UA-54083301-21',
-    'www.olx.com.mx': 'UA-54083301-36',
-    'www.olx.com.ve': 'UA-54083301-1',
-    'www.olx.com.co': 'UA-54083301-27',
-    'www.olx.com.pe': 'UA-54083301-17',
-    'www.olx.com.ec': 'UA-54083301-18',
-    'www.olx.cl': 'UA-54083301-29',
-    'www.olx.com.gt': 'UA-54083301-32',
-    'www.olx.com': 'UA-54083301-30',
-    'www.olx.com.sv': 'UA-54083301-28',
-    'www.olx.com.bd': 'UA-54083301-8',
-    'www.olx.es': 'UA-54083301-37',
-    'www.olx.co.ke': 'UA-54083301-23',
-    'www.jaovat.com': 'UA-54083301-48',
-    'www.olx.com.ng': 'UA-54083301-24',
-    'www.olx.fr': 'UA-54083301-41',
-    'www.olx.com.pa': 'UA-54083301-33',
-    'www.olx.it': 'UA-54083301-46',
-    'www.olx.co.cr': 'UA-54083301-31',
-    'www.olx.com.bo': 'UA-54083301-19',
-    'www.olx.com.gh': 'UA-54083301-25',
-    'www.olx.com.uy': 'UA-54083301-40',
-    'www.olx.com.py': 'UA-54083301-38',
-    'www.olx.hn': 'UA-54083301-39',
-    'www.olx.co.ug': 'UA-54083301-35',
-    'www.olx.com.ni': 'UA-54083301-26',
-    'www.olx.com.au': 'UA-54083301-43',
-    'www.olx.co.tz': 'UA-54083301-34',
-    'www.olx.cm': 'UA-54083301-45',
-    'www.olx.bz': 'UA-54083301-47',
-    'www.olx.ba': 'UA-54083301-49',
-    'www.olx.sn': 'UA-54083301-42'
+    'default': {
+        'default': 'UA-54083301-44',
+        desktop: 'UA-54083301-44' 
+    },
+    development: {
+        'default': 'UA-50756825-1',
+        desktop: 'UA-50756825-1'
+    },
+    staging: {
+        'default': 'UA-50756825-1',
+        desktop: ''
+    },
+    testing: {
+        'default': 'UA-50756825-1',
+        desktop: ''
+    },
+    'www.olx.com.br': {
+        'default': 'UA-54083301-20',
+        desktop: 'UA-137146-17' 
+    },
+    'www.olx.in': {
+        'default': 'UA-54083301-2',
+        desktop: 'UA-137146-4'
+    },
+    'www.olx.com.ar': {
+        'default': 'UA-54083301-22',
+        desktop: 'UA-137146-10'
+    },
+    'www.olx.com.pk': {
+        'default': 'UA-54083301-6',
+        desktop: 'UA-137146-27'   
+    },
+    'www.olx.co.za': {
+        'default': 'UA-54083301-21',
+        desktop: 'UA-137146-11'
+    },
+    'www.olx.com.mx': {
+        'default': 'UA-54083301-36',
+        desktop: 'UA-137146-9'
+    }, 
+    'www.olx.com.ve': {
+        'default': 'UA-54083301-1',
+        desktop: 'UA-137146-33'
+    },
+    'www.olx.com.co': {
+        'default': 'UA-54083301-27',
+        desktop: 'UA-137146-20'
+    },
+    'www.olx.com.pe': {
+        'default': 'UA-54083301-17',
+        desktop: 'UA-137146-29'
+    },
+    'www.olx.com.ec': {
+        'default': 'UA-54083301-18',
+        desktop: 'UA-137146-21'
+    },
+    'www.olx.cl': {
+        'default': 'UA-54083301-29',
+        desktop: 'UA-137146-19'
+    },
+    'www.olx.com.gt': {
+        'default': 'UA-54083301-32',
+        desktop: 'UA-137146-84'
+    },
+    'www.olx.com': {
+        'default': 'UA-54083301-30',
+        desktop: 'UA-137146-2'
+    },
+    'www.olx.com.sv': {
+        'default': 'UA-54083301-28',
+        desktop: 'UA-137146-100'
+    },
+    'www.olx.com.bd': {
+        'default': 'UA-54083301-8',
+        desktop: 'UA-137146-99'
+    },
+    'www.olx.es': {
+        'default': 'UA-54083301-37',
+        desktop: 'UA-137146-13'
+    },
+    'www.olx.co.ke': {
+        'default': 'UA-54083301-23',
+        desktop: 'UA-137146-110'
+    },
+    'www.jaovat.com': {
+        'default': 'UA-54083301-48',
+        desktop: 'UA-137146-70'
+    },
+    'www.olx.com.ng': {
+        'default': 'UA-54083301-24',
+        desktop: 'UA-137146-111'
+    },
+    'www.olx.fr': {
+        'default': 'UA-54083301-41',
+        desktop: 'UA-137146-23'
+    },
+    'www.olx.com.pa': {
+        'default': 'UA-54083301-33',
+        desktop: 'UA-137146-102'
+    },
+    'www.olx.it': {
+        'default': 'UA-54083301-46',
+        desktop: 'UA-137146-38'
+    },
+    'www.olx.co.cr': {
+        'default': 'UA-54083301-31',
+        desktop: 'UA-137146-81'
+    },
+    'www.olx.com.bo': {
+        'default': 'UA-54083301-19',
+        desktop: 'UA-137146-80'
+    },
+    'www.olx.com.gh': {
+        'default': 'UA-54083301-25',
+        desktop: 'UA-137146-114'
+    },
+    'www.olx.com.uy': {
+        'default': 'UA-54083301-40',
+        desktop: 'UA-137146-35'
+    },
+    'www.olx.com.py': {
+        'default': 'UA-54083301-38',
+        desktop: 'UA-137146-28'
+    },
+    'www.olx.hn': {
+        'default': 'UA-54083301-39',
+        desktop: 'UA-137146-85'
+    },
+    'www.olx.co.ug': {
+        'default': 'UA-54083301-35',
+        desktop: ''
+    },
+    'www.olx.com.ni': {
+        'default': 'UA-54083301-26',
+        desktop: 'UA-137146-93'
+    },
+    'www.olx.com.au': {
+        'default': 'UA-54083301-43',
+        desktop: ''
+    },
+    'www.olx.co.tz': {
+        'default': 'UA-54083301-34',
+        desktop: 'UA-137146-117'
+    },
+    'www.olx.cm': {
+        'default': 'UA-54083301-45',
+        desktop: 'UA-137146-120'
+    },
+    'www.olx.bz': {
+        'default': 'UA-54083301-47',
+        desktop: ''
+    },
+    'www.olx.ba': {
+        'default': 'UA-54083301-49',
+        desktop: 'UA-137146-109'
+    },
+    'www.olx.sn': {
+        'default': 'UA-54083301-42',
+        desktop: 'UA-137146-121'
+    }
 };
