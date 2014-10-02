d817002ef555db9d23abd31384ef243dc714bbb9
Andres Risso <andresr@olx.com>
2014-10-02 15:44:54 -0300

    [MOB-6573] Sort implementation
    Cleaned up views

--- app/localized/common/app/views/items/sort.js
+++ app/localized/common/app/views/items/sort.js
@@ -1,24 +1,12 @@
 'use strict';
 
 var Base = require('../../bases/view');
-var _ = require('underscore');
-var querystring = require('querystring');
-var asynquence = require('asynquence');
-var helpers = require('../../../../../helpers');
-var analytics = require('../../../../../modules/analytics');
 
 module.exports = Base.extend({
     className: 'items_sort_view',
     wapAttributes: {
         cellpadding: 0
-    },
-    getTemplateData: function() {
-        var data = Base.prototype.getTemplateData.call(this);
-
-        return _.extend({}, data, {
-            
-        });
-    },
+    }
 });
 
 module.exports.id = 'items/sort';
