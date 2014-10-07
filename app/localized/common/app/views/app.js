0a2a09029908744c32d6630860d2549509b440b8
Roberto Braile <roberto.braile@olx.com>
2014-10-07 13:11:06 -0300

    [expedition/desktop] - create modal and functionality

--- app/localized/common/app/views/app.js
+++ app/localized/common/app/views/app.js
@@ -5,6 +5,10 @@ var URLParser = require('url');
 
 module.exports = Base.extend({
     className: 'app_view',
+    events: {
+        'click .modal-close': 'closeModal',
+        'click .title-bar a': 'openModal'
+    },
     initialize: function() {
         this.app.on('change:loading', this.loading.bind(this, this.$('#progressBar')));
         window.initTracker();
@@ -34,7 +38,40 @@ module.exports = Base.extend({
             e.preventDefault();
             this.app.router.redirectTo(href);
         }
+    },
+    openModal: function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+        showModal(event.currentTarget.dataset.modal);
+    },
+    closeModal: function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+        hideModal(event.currentTarget.dataset.modal);
     }
 });
+
+function showModal(classToShow) {
+    var windowHeight = window.innerHeight,
+        modal = $('.' + classToShow),
+        top = 0,
+        modalHeight = modal.height();
+    if(modalHeight < windowHeight){
+        top = (windowHeight/2) -(modalHeight/2);
+    }
+    modal.css('top',top);
+
+    $('body').css('overflow','hidden');
+    $('#modal-overlay').removeClass('modal-hide');
+    $('.' + classToShow).removeClass('modal-hide');
+}
+
+function hideModal(classToHide) {
+    $('body').css('overflow','auto');
+    $('#modal-overlay').addClass('modal-hide');
+    $('.' + classToHide).addClass('modal-hide');
+}
 
 module.exports.id = 'app_view/index';
