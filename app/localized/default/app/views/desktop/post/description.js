0dee898c4b63880b98d5b402605ebd3f20956d0f
Mauricio Scardaccione <mauricio.scardaccione@olx.com>
2014-10-08 14:42:51 -0300

    [ONEWEB-48] DESKTOP : DEV :: Posting :: Files Structure and markup

--- app/localized/default/app/views/desktop/post/description.js
+++ app/localized/default/app/views/desktop/post/description.js
@@ -6,8 +6,8 @@ var _ = require('underscore');
 
 module.exports = Base.extend({
     tagName: 'section',
-    id: 'posting-contact-view',
-    className: 'posting-contact-view'
+    id: 'posting-description-view',
+    className: 'posting-description-view'
 });
 
-module.exports.id = 'post/contact';
+module.exports.id = 'post/description';
