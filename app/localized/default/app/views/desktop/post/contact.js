6e167e58b0a9c1901718e06151f97a9f55834c07
Mauricio Scardaccione <mauricio.scardaccione@olx.com>
2014-10-14 18:10:02 -0300

    [ONEWEB-136] DESKTOP :: Posting Flow :: Validations

--- app/localized/default/app/views/desktop/post/contact.js
+++ app/localized/default/app/views/desktop/post/contact.js
@@ -7,7 +7,42 @@ var _ = require('underscore');
 module.exports = Base.extend({
     tagName: 'section',
     id: 'posting-contact-view',
-    className: 'posting-contact-view'
+    className: 'posting-contact-view',
+    validations: {},
+    events: {
+        'update': 'onUpdate',
+        'validate': 'onValidate'
+    },
+
+    onUpdate: function(event, fields) {
+        event.preventDefault();
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+
+        _.each(fields, function(field) {
+            this.validations[field.name] = field.validations;
+        }, this);
+    },
+    onValidate: function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+
+        _.each(this.validations, function(validations, field) {
+            _.each(validations, function(validation) {
+                if (field === 'email') {
+                    if (validation.match) {
+                        if (!$('#seller-email').val().match(eval(validation.match))) {
+                            alert('Invalid');
+                        }
+                        else {
+                            alert('Valid');
+                        }
+                    }
+                }
+            });
+        });
+    }
 });
 
 module.exports.id = 'post/contact';
