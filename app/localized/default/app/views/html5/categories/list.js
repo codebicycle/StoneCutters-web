75e8e4ef311635ad3583dcb5541b2126d5f0d8f1
cristianele <cristian.elena@olx.com>
2014-10-14 17:19:37 -0300

    MOB-6588 - Refresh Home

---
+++ app/localized/default/app/views/html5/categories/list.js
@@ -0,0 +1,33 @@
+var Base = require('../../../../../common/app/bases/view').requireView('categories/list');
+var _ = require('underscore');
+
+module.exports = Base.extend({
+    className: 'categories_list_view',
+    events: {
+        'click #categories .category > h2 > a': 'expandSubCat'
+    },
+    getTemplateData: function() {
+        var data = Base.prototype.getTemplateData.call(this);
+
+        return _.extend({}, data);
+    },
+    expandSubCat: function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+
+        var className = 'expanded';
+        var currentCategory = $(event.target).parents('.category');
+        var allCategories = this.$('.category');
+        var allSubCategories = this.$('.subcategories');
+
+        if(!currentCategory.hasClass(className)){
+            allCategories.removeClass(className);
+            allSubCategories.stop(1, 1).slideUp();
+            currentCategory.addClass(className).find('.subcategories').stop(1, 1).slideDown();
+        }else {
+            allCategories.removeClass(className);
+            allSubCategories.stop(1, 1).slideUp();
+        }
+    }
+});
