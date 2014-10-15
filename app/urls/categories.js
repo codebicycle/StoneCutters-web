fc085b3e7480ab8e7a32f090359bef660f08aafb
Mauricio Scardaccione <mauricio.scardaccione@olx.com>
2014-10-15 12:38:54 -0300

    [ONEWEB-46] DESKTOP :: DEV :: Listin pages :: Galery view markup

--- app/urls/categories.js
+++ app/urls/categories.js
@@ -1,6 +1,79 @@
 'use strict';
 
 module.exports = {
+    'categories#showig#pageSlug': {
+        urls: {
+            server: ':title-cat-:catId([0-9]+)-p-:page([0-9]+)-ig/?:filters?',
+            client: {
+                url: ':title-cat-:catId-p-:page(/)-ig(:filters)'
+            }
+        }
+    },
+    'categories#showig#pageNoSlug': {
+        urls: {
+            server: '-cat-:catId([0-9]+)-p-:page([0-9]+)-ig/?:filters?',
+            client: {
+                url: '-cat-:catId-p-:page(/)-ig(:filters)'
+            }
+        }
+    },
+    'categories#showig#page': {
+        urls: {
+            server: 'cat-:catId([0-9]+)-p-:page([0-9]+)-ig/?:filters?',
+            client: {
+                url: 'cat-:catId-p-:page(/)-ig(:filters)'
+            }
+        }
+    },
+    'categories#showig#pageSlugWithFilter': {
+        urls: {
+            server: ':title-cat-:catId([0-9]+)-ig/?:filters?',
+            client: {
+                url: ':title-cat-:catId(/)-ig(:filters)'
+            }
+        }
+    },
+    'categories#showig#pageNoSlugWithFilter': {
+        urls: {
+            server: '-cat-:catId([0-9]+)-ig/?:filters?',
+            client: {
+                url: '-cat-:catId(/)-ig(:filters)'
+            }
+        }
+    },
+    'categories#showig#pageWithFilter': {
+        urls: {
+            server: 'cat-:catId([0-9]+)-ig/?:filters?',
+            client: {
+                url: 'cat-:catId(/)-ig(:filters)'
+            }
+        }
+    },
+    'categories#showig#slug': {
+        urls: {
+            server: ':title-cat-:catId([0-9]+)-ig',
+            client: {
+                url: ':title-cat-:catId-ig'
+            }
+        }
+    },
+    'categories#showig#noSlug': {
+        urls: {
+            server: '-cat-:catId([0-9]+)-ig',
+            client: {
+                url: '-cat-:catId-ig'
+            }
+        }
+    },
+    'categories#showig': {
+        urls: {
+            server: 'cat-:catId([0-9]+)-ig',
+            client: {
+                url: 'cat-:catId-ig'
+            }
+        }
+    },
+
     'categories#show#pageSlug': {
         urls: {
             server: ':title-cat-:catId([0-9]+)-p-:page([0-9]+)/?:filters?',
