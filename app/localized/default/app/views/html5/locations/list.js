b9231c332bf465e1192510a962924033201a1a24
Andres Risso <andresr@olx.com>
2014-10-17 14:50:29 -0300

    [ONEWEB-155] Location :: Filters & Home

--- app/localized/default/app/views/html5/locations/list.js
+++ app/localized/default/app/views/html5/locations/list.js
@@ -10,11 +10,29 @@ module.exports = Base.extend({
         this.app.router.once('action:start', this.onStart);
         this.app.router.once('action:end', this.onEnd);        
     },
+    events: {
+        'submit': 'onSubmit'
+    },
     onStart: function(event) {
         this.appView.trigger('location:end');
     },
     onEnd: function(event) {
         this.appView.trigger('location:start');
+    },
+    onSubmit: function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+
+        var url = '/location?search=' + (this.$('form input[name=search]').val() || '');
+
+        if (this.options.target) {
+            url += '&target=' + this.options.target;
+        }
+
+        helpers.common.redirect.call(this.app.router, url, null, {
+            status: 200
+        });
     }
 });
 
