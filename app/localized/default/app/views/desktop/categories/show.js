acd87b06940e2dd61ee9d34706af2ae5f67fb5e9
Mauricio Scardaccione <mauricio.scardaccione@olx.com>
2014-10-17 13:10:20 -0300

    [ONEWEB-45] DESKTOP :: DEV :: Listin pages :: Filters functionality

--- app/localized/default/app/views/desktop/categories/show.js
+++ app/localized/default/app/views/desktop/categories/show.js
@@ -8,13 +8,15 @@ module.exports = Base.extend({
     id: 'categories-show-view',
     className: 'categories-show-view',
     tagName: 'main',
+    events: {
+        'click .check-box input': 'filterCheckbox'
+    },
     getTemplateData: function() {
         var data = Base.prototype.getTemplateData.call(this);
         var slugUrl = helpers.common.slugToUrl(data.currentCategory);
         var filters = data.metadata.filters;
-        var order = ['pricerange','hasimage','state','parentcategory'];
+        var order = ['pricerange','carbrand','condition','kilometers','sellertype','year'];
         var list = [];
-        console.log(filters);
 
         _.each(order, function(obj, i){
             _.find(filters, function(obj){
@@ -22,19 +24,35 @@ module.exports = Base.extend({
             });
         });
 
-        console.log(list);
-
         _.each(data.items, this.processItem);
         return _.extend({}, data, {
             items: data.items,
+            filters: list,
             nav: {
                 link: slugUrl,
-                listAct: 'active'
+                listAct: 'active',
             }
         });
     },
     processItem: function(item) {
         item.date.since = helpers.timeAgo(item.date);
+    },
+    filterCheckbox: function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+
+        var filterName = $(event.currentTarget).data('filter');
+        var filterValue = $(event.currentTarget).val();
+        var currentFilter = '/-' + filterName + '_' + filterValue;
+        var currentUrl = window.location.pathname;
+        var ulrParts = currentUrl.split('/');
+
+        currentUrl = currentUrl.split('/-')[0];
+
+        var path = helpers.common.link(currentUrl + currentFilter, this.app);
+
+        this.app.router.redirectTo(path);
     }
 });
 
