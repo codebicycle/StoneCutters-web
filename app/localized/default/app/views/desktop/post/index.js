dbca217f2131cb9fb9164a30202f45fb6bf20af5
Gaston Lasarte <gaston.lasarte@olx.com>
2014-10-20 17:37:14 -0300

    ONEWEB-39 - Improved focus behavour

--- app/localized/default/app/views/desktop/post/index.js
+++ app/localized/default/app/views/desktop/post/index.js
@@ -10,36 +10,20 @@ module.exports = Base.extend({
     className: 'posting-view',
     events: {
         'focus .text-field': 'onFocus',
-        'blur .text-field': 'onBlur',
-        'click .posting': 'onSubmit'
+        'blur .text-field': 'onFocus'
     },
 
     getTemplateData: function() {
         var data = Base.prototype.getTemplateData.call(this);
         return _.extend({}, data);
     },
-    onBlur: function(event) {
-        event.preventDefault();
-        var $input = $(event.currentTarget);
-        var $container = $input.closest('.wrapper');
-        var $removeFocus = $container.removeClass('input-focus');
-
-        $removeFocus.blur();
-    },
     onFocus: function(event) {
         event.preventDefault();
         var $input = $(event.currentTarget);
         var $container = $input.closest('.wrapper');
-        var $addFocus = $container.addClass('input-focus');
+        var $toggleFocus = $container.toggleClass('input-focus');
 
-        $addFocus.focus();
-    },
-    onSubmit: function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        event.stopImmediatePropagation();
-
-        this.$('#posting-contact-view').trigger('validate');
+        $toggleFocus.focus();
     }
 });
 
