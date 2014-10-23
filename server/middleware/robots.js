689d4445a7fad63017cb4b569db5f3f3050670a6
Martin Di Domenico <martin.didomenico@olx.com>
2014-10-23 16:46:20 -0300

    [ONEWEB-6] Robots.txt
    Added desktop robots

--- server/middleware/robots.js
+++ server/middleware/robots.js
@@ -2,15 +2,18 @@
 
 module.exports = function(dataAdapter, excludedUrls) {
     return function loader() {
+        var _ = require('underscore');
         var path = require('path');
-        var robots = path.resolve('server/templates/robots.txt');
+        var robots = path.resolve('server/templates/robots/web.txt');
+        var mRobots = path.resolve('server/templates/robots/mobile.txt');
+        var mobile = ['m', 'wap', 'html4', 'html5'];
 
         return function middleware(req, res, next) {
             if (req.path !== '/robots.txt') {
                 return next();
             }
             res.set('Content-Type', 'text/plain');
-            res.status(200).sendfile(robots);
+            res.status(200).sendfile(_.contains(mobile, req.subdomains.pop()) ? mRobots : robots);
         };
     };
 };
