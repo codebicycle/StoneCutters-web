51e688e825bc53931062ebc96f719914dd4f9844
Nicolas Molina <nicolas.molina@olx.com>
2014-10-23 11:54:51 -0300

    Tracking :: Fixes bug

--- app/localized/default/app/views/desktop/partials/tracking.js
+++ app/localized/default/app/views/desktop/partials/tracking.js
@@ -43,7 +43,7 @@ module.exports = Base.extend({
         event.stopPropagation();
         event.stopImmediatePropagation();
 
-        if (tracking) {
+        if (tracking && tracking.params) {
             this.$el.trigger('trackAti', tracking);
             this.$el.trigger('trackAnalytics', tracking);
             this.$el.trigger('trackHydra', tracking);
