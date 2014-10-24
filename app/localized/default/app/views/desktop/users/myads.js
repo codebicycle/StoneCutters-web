c1e4015e373773a34c001f90ba1bf1adf07a622c
Abrahan Apaza <abrahan.apaza@olx.com>
2014-10-24 09:54:06 -0300

    [ONEWEB-96] MyAds : remove ads and favorites in common view

---
+++ app/localized/default/app/views/desktop/users/myads.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var Base = require('../../bases/view');
+var _ = require('underscore');
+var helpers = require('../../../../../helpers');
+
+module.exports = Base.extend({
+    className: 'users_myads_view',
+    getTemplateData: function() {
+        var data = Base.prototype.getTemplateData.call(this);
+        return _.extend({}, data, {
+            breadcrumb: helpers.breadcrumb.get.call(this, data),
+            myAds: data.context.ctx.myAds
+        });
+    }
+});
+
+module.exports.id = 'users/myads';
