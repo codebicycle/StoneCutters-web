9645b75cb4a36d90760c4d369287ecd25288916a
Abrahan Apaza <abrahan.apaza@olx.com>
2014-10-24 10:29:47 -0300

    [ONEWEB-96] MyAds : remove view from common

--- app/localized/default/app/views/desktop/users/myads.js
+++ app/localized/default/app/views/desktop/users/myads.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var Base = require('../../bases/view');
+var Base = require('../../../../../common/app/bases/view');
 var _ = require('underscore');
-var helpers = require('../../../../../helpers');
+var helpers = require('../../../../../../helpers');
 
 module.exports = Base.extend({
     className: 'users_myads_view',
