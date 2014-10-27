40daa954ca9f55feca9ea99983040e1477e7bc6d
Gonzalo Aizpún <gonzalo.aizpun@olx.com>
2014-10-27 17:00:24 -0300

    fix merge

--- app/localized/default/app/views/html5/footer/languages.js
+++ app/localized/default/app/views/html5/footer/languages.js
@@ -10,6 +10,16 @@ module.exports = Base.extend({
     getTemplateData: function() {
         var data = Base.prototype.getTemplateData.call(this);
         var currentRoute = this.app.session.get('currentRoute');
+        var languages = this.app.session.get('languages').models;
+        var selected = this.app.session.get('selectedLanguage');
+
+        var selectedLanguage = _.find(languages, function(language){
+            return language.locale === selected;
+        });
+
+        var languagesList = _.filter(languages, function(language){
+            return language.locale !== selected;
+        });
 
         return _.extend({}, data, {
             languages: {
@@ -20,11 +30,23 @@ module.exports = Base.extend({
             selectedLanguage: this.app.session.get('selectedLanguage')
         });
     },
+    events: {
+        'click strong.drop': 'languageToggle'
+    },
     postRender: function() {
         $('body').on('change:location', this.changeLocation.bind(this));
-        this.app.router.appView.on('postingflow:start', this.onPostingFlowStart.bind(this));
-        this.app.router.appView.on('postingflow:end', this.onPostingFlowEnd.bind(this));
-        this.app.router.on('action:end', this.onActionEnd.bind(this));
+    },
+    languageToggle: function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+        var btn = $(event.target);
+        if (btn.hasClass('open')) {
+            btn.removeClass('open').addClass('close');
+        } else if (btn.hasClass('close')) {
+            btn.removeClass('close').addClass('open');
+        }
+        $('.footer-links').slideToggle();
     },
     changeLocation: function (e, siteLocation) {
         this.$('.footer-links .footer-link').each(function(i, link) {
@@ -46,15 +68,6 @@ module.exports = Base.extend({
                 });
             }
         }.bind(this));
-    },
-    onPostingFlowStart: function() {
-        this.$('#languages').addClass('disabled');
-    },
-    onPostingFlowEnd: function() {
-        this.app.router.once('action:end', this.onPostingFlowAfter.bind(this));
-    },
-    onPostingFlowAfter: function() {
-        this.$('#languages').removeClass('disabled');
     },
     onActionEnd: function(e, loading) {
         this.$('#languages .footer-links a').each(function(i, link) {
