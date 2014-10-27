2ba3c5d077719d6b5e8a197da249da5e30624c55
cristianele <cristian.elena@olx.com>
2014-10-27 17:31:07 -0300

    Update model

--- app/models/country.js
+++ app/models/country.js
@@ -4,13 +4,7 @@ var Base = require('../bases/model');
 
 module.exports = Base.extend({
     idAttribute: 'url',
-    url: '/countries/:code',
-    parse: function(result) {
-        var Collection = require('../collections/countries');
-
-        result.children = new Collection(result.children);
-        return result;
-    }
+    url: '/countries/:code'
 });
 
 module.exports.id = 'Country';
