b85313c0233a5afbad148a8ed0006794f446d660
cristianele <cristian.elena@olx.com>
2014-10-27 17:48:53 -0300

    Remove requires

--- server/middleware/countries.js
+++ server/middleware/countries.js
@@ -8,10 +8,7 @@ module.exports = function(dataAdapter) {
         var asynquence = require('asynquence');
         var config = require('../config');
         var statsd  = require('../modules/statsd')();
-        var utils = require('../../shared/utils');
         var errorPath = path.resolve('server/templates/error.html');
-        var testing = config.get(['publicEnvironments', 'testing'], {});
-        var staging = config.get(['publicEnvironments', 'staging'], {});
 
         return function middleware(req, res, next) {
     
