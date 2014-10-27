f8c5542756a7d91dbb017a3efb508ef8312695c7
cristianele <cristian.elena@olx.com>
2014-10-27 12:38:42 -0300

    [ONEWEB-94] - Sitemap view - Extend categorytree

--- app/localized/common/app/views/pages/sitemap.js
+++ app/localized/common/app/views/pages/sitemap.js
@@ -4,7 +4,26 @@ var Base = require('../../bases/view');
 var _ = require('underscore');
 
 module.exports = Base.extend({
-    className: 'pages-sitemap-view'
+    tagName: "section",
+    className: 'pages-sitemap-view',
+    id: "category-tree",    
+    getTemplateData: function() {
+        var data = Base.prototype.getTemplateData.call(this);
+        var categories = data.categories;
+        var order = ['For Sale','Classes','Vehicles','Community','Real Estate','Services','Jobs'];
+        var list = [];
+
+        _.each(order, function(obj, i){
+            _.find(categories, function(obj){
+                return obj.name == order[i] ? list.push(obj) : false;
+            });
+        });
+
+        return _.extend({}, data, {
+            location: this.app.session.get('location'),
+            categories: list
+        });
+    }
 });
 
 module.exports.id = 'pages/sitemap';
