13fc04f29cc845e8ac27da66d61a317224f82741
Mauricio Scardaccione <mauricio.scardaccione@olx.com>
2014-10-30 15:13:28 -0300

    Remove semicolon

--- app/localized/default/app/views/desktop/users/register.js
+++ app/localized/default/app/views/desktop/users/register.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var Base = require('../../../../../common/app/bases/view').requireView('users/register');;
+var Base = require('../../../../../common/app/bases/view').requireView('users/register');
 var helpers = require('../../../../../../helpers');
 var _ = require('underscore');
 
