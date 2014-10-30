41c7ba0691b4c6ca4fd18c8bb9e835aecd3e5646
Juan Baragli <juan.baragli@olx.com>
2014-10-30 10:42:23 -0300

    [expedition/desktop-ONEWEB-95] remove console.log

--- app/localized/default/app/views/desktop/users/login.js
+++ app/localized/default/app/views/desktop/users/login.js
@@ -9,7 +9,7 @@ module.exports = Base.extend({
     getTemplateData: function() {
         var data = Base.prototype.getTemplateData.call(this);
         var params = this.options.params || {};
-        console.log(data.form);
+
         return _.extend({}, data, {
             params: params
         });
