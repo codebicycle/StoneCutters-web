a635e9bbf2a68cb98a04b466d4a19970da3b4525
Nicolas Molina <nicolas.molina@olx.com>
2014-10-30 12:19:32 -0300

    Desktop :: Filters :: Merge

--- app/helpers/common.js
+++ app/helpers/common.js
@@ -104,7 +104,6 @@ module.exports = (function() {
         }
         seo.addMetatag('robots', 'noindex, nofollow');
         seo.addMetatag('googlebot', 'noindex, nofollow');
-        console.log(err.stack);
         return callback(null, 'pages/error', res || {});
     }
 
