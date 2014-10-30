00d21b1a14eaaf3247efa9df3235f231c883ce7c
Juan Baragli <juan.baragli@olx.com>
2014-10-30 15:07:51 -0300

    [expedition/desktop-ONEWEB-95]

--- app/localized/default/app/views/desktop/users/register.js
+++ app/localized/default/app/views/desktop/users/register.js
@@ -1,9 +1,8 @@
 'use strict';
 
-var Base = require('../../../../../common/app/bases/view');
+var Base = require('../../../../../common/app/bases/view').requireView('users/register');;
 var helpers = require('../../../../../../helpers');
 var _ = require('underscore');
-var asynquence = require('asynquence');
 
 module.exports = Base.extend({
     className: 'users_register_view',
@@ -24,5 +23,3 @@ module.exports = Base.extend({
     }
 
 });
-
-module.exports.id = 'users/register';
