8812bf7eb80b916e922dfea06366e17d1056b262
Mauricio Scardaccione <mauricio.scardaccione@olx.com>
2014-11-03 12:03:20 -0300

    After a reply the success message is not shown
    Reply - Telephone field is not cleared after sending a message
    Reply - "Mensaje" information text color is different from the other ones

--- app/localized/default/app/views/desktop/items/show.js
+++ app/localized/default/app/views/desktop/items/show.js
@@ -133,12 +133,12 @@ module.exports = Base.extend({
             $('.name').val('');
             $('.email').val('');
             $('.phone').val('');
+            $replySuccess.removeClass('hide');
             this.track({
                 category: 'Reply',
                 action: 'ReplySuccess',
                 custom: ['Reply', category, subcategory, 'ReplySuccess', itemId].join('::')
             });
-            $replySuccess.removeClass('hide');
             done(data);
         }.bind(this);
 
