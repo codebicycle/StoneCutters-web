da2df59c09ea49cd9c4b1a8dad83b5ea2256a5b2
Abrahan Apaza <abrahan.apaza@olx.com>
2014-11-03 12:33:46 -0300

    [ONEWEB-249b] Hide footer categories in Home

--- app/localized/default/app/views/desktop/header/index.js
+++ app/localized/default/app/views/desktop/header/index.js
@@ -10,6 +10,8 @@ module.exports = Base.extend({
     postRender: function () {
         this.app.router.appView.on('posting:start', this.onPostingStart.bind(this));
         this.app.router.appView.on('posting:end', this.onPostingEnd.bind(this));
+        this.app.router.appView.on('home:start', this.onHomeStart.bind(this));
+        this.app.router.appView.on('home:end', this.onHomeEnd.bind(this));
     },
     onPostingStart: function () {
         $('.posting, .search-form').addClass('disabled');
@@ -18,5 +20,11 @@ module.exports = Base.extend({
     onPostingEnd: function () {
         $('.posting, .search-form').removeClass('disabled');
         $('.posting-title').addClass('disabled');
+    },
+    onHomeStart: function () {
+        $('.footer-options .categories').addClass('hide');
+    },
+    onHomeEnd: function () {
+        $('.footer-options .categories').removeClass('hide');
     }
 });
