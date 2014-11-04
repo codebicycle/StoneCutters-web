1f948b3991faae0ef8743b6455858c28b79f2d77
Gaston Lasarte <gaston.lasarte@olx.com>
2014-11-04 11:26:37 -0300

    Changed linkig function for link and the color of the icon close in listing breadcrumb

--- app/modules/nunjucks/helpers.js
+++ app/modules/nunjucks/helpers.js
@@ -135,6 +135,9 @@ module.exports = function(nunjucks) {
         if (this.ctx.nav && this.ctx.nav.galeryAct) {
             href = helpers.common.linkig.call(this.ctx, href, query, this.ctx.nav.current);
         }
+        else {
+            return link.apply(this, arguments);
+        }
         return helpers.common.fullizeUrl(href, this.ctx.app);
     }
 
