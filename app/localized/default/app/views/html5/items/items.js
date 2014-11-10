8927ad960a0c1f1da9778b026e8dec377df6666a
Abrahan Apaza <abrahan.apaza@olx.com>
2014-11-10 16:52:05 -0300

    [ONEWEB-175b] remove console

--- app/localized/default/app/views/html5/items/items.js
+++ app/localized/default/app/views/html5/items/items.js
@@ -18,6 +18,7 @@ module.exports = Base.extend({
     },
     getTemplateData: function() {
         var data = Base.prototype.getTemplateData.call(this);
+
         return _.extend({}, data, {
             items: this.items
         });
