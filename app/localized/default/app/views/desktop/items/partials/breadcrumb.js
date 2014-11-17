a385f968849b948c9e0e313e4a314f4fc957c0e3
Mauricio Scardaccione <mauricio.scardaccione@olx.com>
2014-11-17 17:33:24 -0300

    Templates to Views

---
+++ app/localized/default/app/views/desktop/items/partials/breadcrumb.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Base = require('../../../../../../common/app/bases/view');
+
+module.exports = Base.extend({
+    className: 'items-breadcrumb',
+    id: 'items-breadcrumb',
+    tagName: 'nav',
+});
+
+module.exports.id = 'items/partials/breadcrumb';
