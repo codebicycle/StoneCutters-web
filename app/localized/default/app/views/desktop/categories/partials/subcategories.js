e8c058d3057e416d38abd40218f851e2c3a909a7
Mauricio Scardaccione <mauricio.scardaccione@olx.com>
2014-11-18 11:55:27 -0300

    Categories refactor

---
+++ app/localized/default/app/views/desktop/categories/partials/subcategories.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var Base = require('../../../../../../common/app/bases/view');
+
+module.exports = Base.extend({
+    className: 'listing-subcategories',
+    id: 'listing-subcategories'
+});
+
+module.exports.id = 'categories/partials/subcategories';
