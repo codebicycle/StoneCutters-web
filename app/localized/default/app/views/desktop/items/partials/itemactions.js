bcb607ccc73ee8115e8220bc4e39ef99814232d0
Paula Iglesias <paula.iglesias@olx.com>
2014-11-18 10:28:35 -0300

    ONEWEB-336 - Pop-up login in item page

--- app/localized/default/app/views/desktop/items/partials/itemactions.js
+++ app/localized/default/app/views/desktop/items/partials/itemactions.js
@@ -1,18 +1,23 @@
 'use strict';
 
 var Base = require('../../../../../../common/app/bases/view');
+var _ = require('underscore');
 var helpers = require('../../../../../../../helpers');
+var asynquence = require('asynquence');
+var User = require('../../../../../../../models/user');
 
 module.exports = Base.extend({
     className: 'item-actions',
     id: 'item-actions',
     events: {
-        'click [data-fav]': 'addToFavorites'
+        'click [data-fav]': 'addToFavorites',
+        'submit [data-login-form]': 'login'
     },
     addToFavorites: function (e) {
         var $this = $(e.currentTarget);
+        var dataUser = $this.data('user');
 
-        if ($this.attr('href') == '#') {
+        if (dataUser) {
             e.preventDefault();
             var user = this.app.session.get('user');
             var itemId = $this.data('itemid');
@@ -53,6 +58,66 @@ module.exports = Base.extend({
                 $this.attr('href', '#');
             });
         }
+    },
+    login: function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+        
+        var data = {
+            usernameOrEmail: this.$('[name="usernameOrEmail"]').val(),
+            password: this.$('[name="password"]').val()
+        };
+        var user;
+        
+        function prepare(done) {
+            user = new User(_.extend(data, {
+                location: this.app.session.get('siteLocation'),
+                country: this.app.session.get('location').name,
+                languageId: this.app.session.get('languages')._byId[this.app.session.get('selectedLanguage')].id,
+                platform: this.app.session.get('platform')
+            }), {
+                app: this.app
+            });
+            done();
+        }
+
+        function submit(done) {
+            user.login(done);
+        }
+
+        function success(res) {
+            $('[data-modal-close]').click();
+            console.log('success');
+            console.log(res);
+            // var redirect = user.get('redirect');
+
+            // if (!redirect || redirect.match(/(\/register|\/login|\/logout)/g)) {
+            //     redirect = '/';
+            // }
+            // res.redirect(utils.link(redirect, this.app));
+            // end();
+        }
+
+        function error(err) {
+            console.log('error');
+            console.log(err);
+            // var link = '/login';
+            // var redirect = user ? user.get('redirect') : '';
+
+            // if (redirect && redirect.match(/(\/register|\/login|\/logout|\/)/g)) {
+            //     link += '?redirect=' + redirect;
+            // }
+            // formidable.error(req, link, err, user.toJSON(), function redirect(url) {
+            //     res.redirect(utils.link(url, req.rendrApp));
+            //     end(err);
+            // });
+        }
+
+        asynquence().or(error.bind(this))
+            .then(prepare.bind(this))
+            .then(submit.bind(this))
+            .val(success.bind(this));
     }
 });
 
