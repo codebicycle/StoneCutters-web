0588a609a09b10c5628315266046c33fb7373add
Guillermo Maquieira <guillermom@olx.com>
2014-11-19 18:19:06 -0300

    Tracking :: GA :: SeoKwGA

--- app/controllers/items.js
+++ app/controllers/items.js
@@ -895,6 +895,10 @@ function staticSearch(params, callback, gallery) {
             tracking.addParam('category', category ? category.toJSON() : undefined);
             tracking.addParam('subcategory', subcategory ? subcategory.toJSON() : undefined);
 
+            if (!query.search) {
+                console.log('[OLX_DEBUG]', 'tracker analytics keyword', '|', 'url', '|', this.app.session.get('url'), '|', 'referer', '|', this.app.session.get('referer'));
+            }
+
             callback(null, ['items/staticsearch', (gallery || '').replace('-', '')].join(''), {
                 items: items.toJSON(),
                 meta: meta,
