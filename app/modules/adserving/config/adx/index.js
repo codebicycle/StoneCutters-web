935ede6a50aa5b89bc64a609510f16b7e120c2fe
cristianele <cristian.elena@olx.com>
2014-11-20 11:21:56 -0300

    Phase 2

--- app/modules/adserving/config/adx/index.js
+++ app/modules/adserving/config/adx/index.js
@@ -1,13 +1,13 @@
 'use strict';
 
 module.exports = {
-    enabled : true,
+    enabled: true,
     options: {
-        google_ad_client : "ca-pub-9177434926134739",
-        google_ad_slot : "8628059986"
+        pubId: "ca-pub-9177434926134739",
+        google_ad_slot: "8628059986"
     },
     params: {
-        width : 300,
-        height : 250
+        width: 300,
+        height: 250
     }
 };
