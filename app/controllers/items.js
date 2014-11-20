f724e2abafc28f4a73d6d66e924d9a6e2841181a
Nicolas Molina <nicolas.molina@olx.com>
2014-11-20 14:58:17 -0300

    Merge

--- app/controllers/items.js
+++ app/controllers/items.js
@@ -202,6 +202,7 @@ function show(params, callback) {
                 else {
                     response.relatedItems = response.relatedItems.toJSON();
                 }
+                console.log(response.relatedItems);
                 done(item, response.relatedItems);
             }.bind(this));
         }.bind(this);
