b36784d6c066d8b0335a1a094cd4ce2e8f5bc06e
Gerardo <gerardo.prieto@olx.com>
2014-12-18 15:56:45 -0300

    added smaug env for every .sh file

--- environments/development.sh
+++ environments/development.sh
@@ -2,10 +2,11 @@
 
 DIR="base"
 CURRENT=${PWD##*/}
+SMAUG="108.171.171.70"
 
 if [ "$CURRENT" != "environments" ]
 then
     DIR="environments/base"
 fi
 
-echo "`./$DIR.sh -i 127.0.0.1 -e Development`"
+echo "`./$DIR.sh -i 127.0.0.1 -e Development -s $SMAUG`"
--- environments/staging.sh
+++ environments/staging.sh
@@ -2,10 +2,11 @@
 
 DIR="base"
 CURRENT=${PWD##*/}
+SMAUG="162.242.207.79"
 
 if [ "$CURRENT" != "environments" ]
 then
     DIR="environments/base"
 fi
 
-echo "`./$DIR.sh -i 162.242.197.57 -e Staging`"
+echo "`./$DIR.sh -i 162.242.197.57 -e Staging -s $SMAUG`"
