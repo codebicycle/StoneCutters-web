958f72dd1c9676f38a0740002629d27bca0859fe
Mauricio Scardaccione <mauricio.scardaccione@olx.com>
2015-01-12 14:37:11 -0300

    Update configs

--- app/config/markets/www.olx.co.tz.js
+++ app/config/markets/www.olx.co.tz.js
@@ -1,4 +1,8 @@
 module.exports = {
+    categoryTree: {
+        order: [185, 186, 187, 362, 16, 191, 190],
+        columns: [1, 3, 2],
+    },
     post_banner: {
         image: 'post-africa'
     }
--- app/config/markets/www.olx.com.pe.js
+++ app/config/markets/www.olx.com.pe.js
@@ -2,6 +2,20 @@ module.exports = {
     post_banner: {
         image: 'post-peru'
     },
+    socials: [
+        {
+            name: 'facebook',
+            link: 'https://www.facebook.com/OLXperu'
+        },
+        {
+            name: 'twitter',
+            link: 'https://twitter.com/OLX_Peru'
+        },
+        {
+            name: 'youtube',
+            link: 'https://www.youtube.com/user/OLXperu'
+        }
+    ],
     videos: [
             {
                 title: 'OLX Peru - Cristian Rivero - Garage',
