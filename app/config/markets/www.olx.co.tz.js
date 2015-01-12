ebb88eb7c1ae478ac991c66a79f26089cbc16319
Mauricio Scardaccione <mauricio.scardaccione@olx.com>
2015-01-12 14:41:00 -0300

    Remove extra commas

--- app/config/markets/www.olx.co.tz.js
+++ app/config/markets/www.olx.co.tz.js
@@ -1,7 +1,7 @@
 module.exports = {
     categoryTree: {
         order: [185, 186, 187, 362, 16, 191, 190],
-        columns: [1, 3, 2],
+        columns: [1, 3, 2]
     },
     post_banner: {
         image: 'post-africa'
--- app/config/markets/www.olx.co.ug.js
+++ app/config/markets/www.olx.co.ug.js
@@ -1,7 +1,7 @@
 module.exports = {
     categoryTree: {
         order: [830, 800, 811, 806, 362, 815, 859, 16, 821],
-        columns: [3, 2, 2],
+        columns: [3, 2, 2]
     },
     post_banner: {
         image: 'post-africa'
