d630fe9ba8fb7be3223db53e5e19501b0cde8a09
Mauricio Scardaccione <mauricio.scardaccione@olx.com>
2015-01-12 11:13:50 -0300

    Update videos config.

---
+++ app/config/markets/www.olx.co.cr.js
@@ -0,0 +1,16 @@
+module.exports = {
+    videos: [
+            {
+                title: 'OLX Costa Rica - Bicicleta - Vender es fácil',
+                id: 'iYUX408RQq4'
+            },
+            {
+                title: 'OLX Costa Rica - Anteojos - Vender es fácil',
+                id: 'Mrfetr0kAoI'
+            },
+            {
+                title: 'OLX Costa Rica - Poker - Vender es fácil',
+                id: 'Yf5i1HOwd70'
+            }
+        ]
+};
---
+++ app/config/markets/www.olx.com.pa.js
@@ -0,0 +1,16 @@
+module.exports = {
+    videos: [
+            {
+                title: 'OLX Panama - Bicicleta - Vender es fácil',
+                id: 'rXfzHDejpRs'
+            },
+            {
+                title: 'OLX Panama - Anteojos - Vender es fácil',
+                id: 'eEyfckF_Xgs'
+            },
+            {
+                title: 'OLX Panamá - Poker - Vender es fácil',
+                id: 'm0sDSvsBEf0'
+            }
+        ]
+};
