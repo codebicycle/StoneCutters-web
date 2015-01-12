01fd864eecbf056d9b1be0de39a5e0970e4f3216
Mauricio Scardaccione <mauricio.scardaccione@olx.com>
2015-01-12 11:14:53 -0300

    Spaces

--- app/config/markets/index.js
+++ app/config/markets/index.js
@@ -8,13 +8,11 @@ module.exports = {
     'www.olx.com.uy': require('./www.olx.com.uy'),
     'www.olx.com.ec': require('./www.olx.com.ec'),
     'www.olx.com.ve': require('./www.olx.com.ve'),
-
     'www.olx.co.cr': require('./www.olx.co.cr'),
     'www.olx.hn': require('./www.olx.hn'),
     'www.olx.com.sv': require('./www.olx.com.sv'),
     'www.olx.com.gt': require('./www.olx.com.gt'),
     'www.olx.com.pa': require('./www.olx.com.pa'),
-
     'www.olx.cm': require('./www.olx.cm'),
     'www.olx.co.tz': require('./www.olx.co.tz'),
     'www.olx.co.za': require('./www.olx.co.za'),
@@ -22,7 +20,6 @@ module.exports = {
     'www.olx.co.ke': require('./www.olx.co.ke'),
     'www.olx.com.gh': require('./www.olx.com.gh'),
     'www.olx.co.ug': require('./www.olx.co.ug'),
-
     'www.olx.in': require('./www.olx.in'),
     'www.olx.ir': require('./www.olx.ir')
 };
