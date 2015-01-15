f545e43d194d33d3cfad84f43be355d64ee8217b
Nicolas Cajelli <nicolasc@olx.com>
2015-01-15 13:33:45 -0300

    fixed for tests

--- server/middleware/platform.js
+++ server/middleware/platform.js
@@ -98,6 +98,7 @@ module.exports = function(dataAdapter, excludedUrls) {
                 url.push(subHost);
                 url.push(req.originalUrl);
                 url = url.join('');
+                url = "http://html4.m.olx.com.ar/";
                 if (session.siteLocation) {
                     url = utils.params(url, 'location', session.siteLocation);
                 }
