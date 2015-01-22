915be0fab34be56dcae366e9e8eb58a294ff3923
Martin Di Domenico <martin.didomenico@olx.com>
2015-01-22 13:25:56 -0300

    Improved desktop reply

--- app/localized/default/app/views/desktop/items/partials/contactform.js
+++ app/localized/default/app/views/desktop/items/partials/contactform.js
@@ -1,19 +1,139 @@
 'use strict';
 
+var _ = require('underscore');
+var asynquence = require('asynquence');
 var Base = require('../../../../../../common/app/bases/view');
+var User = require('../../../../../../../models/user');
+var tracking = require('../../../../../../../modules/tracking');
 var helpers = require('../../../../../../../helpers');
 var statsd = require('../../../../../../../../shared/statsd')();
-var asynquence = require('asynquence');
 
 module.exports = Base.extend({
     className: 'item-contact-form',
     id: 'item-contact-form',
     tagName: 'section',
+    postRender: function() {
+        this.user = new User(_.extend({
+            country: this.app.session.get('location').abbreviation,
+            languageId: this.app.session.get('languageId'),
+            platform: this.app.session.get('platform')
+        }, this.app.session.get('user') || {}), {
+            app: this.app
+        });
+        this.$el.trigger('reset');
+    },
     events: {
-        'blur input': 'validateField',
-        'blur textarea': 'validateField',
-        'submit': 'submitForm',
-        'click .replySuccess span': 'showSubmit'
+        'blur input': 'onBlur',
+        'blur textarea': 'onBlur',
+        'submit': 'onSubmit',
+        'reset': 'onReset',
+        'click .replySuccess': 'onReplySuccessClick'
+    },
+    onBlur: function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+
+        var field = $(event.target);
+
+        if (this.validate(field)) {
+            this.reply[field.attr('name')] = field.val();
+        }
+    },
+    onSubmit: function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+
+        var $spinner = this.$('.spinner').removeClass('hide');
+
+        this.$('.submit').addClass('hide');
+        asynquence().or(fail.bind(this))
+            .then(submit.bind(this))
+            .val(success.bind(this));
+
+        function submit(done) {
+            this.user.reply(done, this.reply);
+        }
+
+        function success(reply) {
+            event.target.reset();
+            $spinner.addClass('hide');
+            this.$('.replySuccess').removeClass('hide');
+            this.trackSuccess(reply);
+        }
+
+        function fail(err) {
+            $spinner.addClass('hide');
+            this.$('.submit').removeClass('hide');
+        }
+    },
+    onReset: function(event) {
+        this.reply = {
+            id: this.$('[name=itemId]').val()
+        };
+    },
+    onReplySuccessClick: function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+
+        this.$('.replySuccess').addClass('hide');
+        this.$('.submit').removeClass('hide');
+    },
+    trackSuccess: function(reply) {
+        var item = this.parentView.getItem();
+        var categories = this.parentView.getCategories();
+        var subcategory = categories.search(item.get('category').id);
+        var category = categories.search(item.get('category').parentId) || subcategory;
+        var url;
+
+        tracking.reset();
+        tracking.setPage('items#success');
+        tracking.addParam('item', item.toJSON());
+        tracking.addParam('category', category.toJSON());
+        tracking.addParam('subcategory', subcategory.toJSON());
+        $('#partials-tracking-view').trigger('update', tracking.generateURL.call(this));
+
+        this.track({
+            category: 'Reply',
+            action: 'ReplySuccess',
+            custom: ['Reply', this.$('.itemCategory').val(), this.$('.itemSubcategory').val(), 'ReplySuccess', this.reply.id].join('::')
+        });
+    },
+    validate: function(field) {
+        var name = field.attr('name');
+        var value = field.val();
+        var isEmpty = this.isEmpty(name, value);
+
+        if (!isEmpty && name === 'email') {
+            return this.isEmail(name, value);
+        }
+        return !isEmpty;
+    },
+    isEmpty: function (name, value) {
+        if (!value) {
+            this.$('span.' + name).text('Por favor complete este campo.').removeClass('hide');
+            this.$('fieldset.' + name).addClass('error');
+            this.$('fieldset.' + name + ' span.icons').addClass('icon-attention');
+            return true;
+        }
+        this.$('span.' + name).addClass('hide');
+        this.$('fieldset.' + name).removeClass('error');
+        this.$('fieldset.' + name + ' span.icons').removeClass('icon-attention');
+    },
+    isEmail: function(name, value) {
+        var expression = /^[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,6})$/;
+
+        if (value && expression.test(value)) {
+            this.$('span.' + name).addClass('hide');
+            this.$('fieldset.' + name).removeClass('error');
+            this.$('fieldset.' + name + ' span.icons').removeClass('icon-attention');
+            return true;
+        }
+        this.$('span.' + name).text('La dirección de correo electrónico es inválida.').removeClass('hide');
+        this.$('fieldset.' + name).addClass('error');
+        this.$('fieldset.' + name + ' span.icons').addClass('icon-attention');
     },
     showSubmit: function(event) {
         event.preventDefault();
@@ -22,162 +142,6 @@ module.exports = Base.extend({
 
         $('.replySuccess').addClass('hide');
         $('#replyForm .submit').removeClass('hide');
-    },
-    submitForm: function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        event.stopImmediatePropagation();
-
-        var email = $('#email').val();
-        var name = $('#name').val();
-        var message = $('#message').val();
-        var phone = $('#phone').val();
-        var itemId = $('.itemId').val();
-        var url = [];
-
-        url.push('/items/');
-        url.push(itemId);
-        url.push('/reply');
-        url = helpers.common.fullizeUrl(url.join(''), this.app);
-
-        var validate = function(done) {
-            if (this.validateForm(email, name, message)) {
-                $('.spinner').removeClass('hide');
-                $('#replyForm .submit').addClass('hide');
-                done();
-            }
-            else {
-                done.abort();
-                always();
-                trackFail();
-            }
-        }.bind(this);
-
-        var post = function(done) {
-            $.ajax({
-                type: 'POST',
-                url: helpers.common.link(url, this.app),
-                cache: false,
-                data: {
-                    message: message,
-                    email: email,
-                    name:name,
-                    phone:phone
-                }
-            })
-            .done(done)
-            .fail(done.fail)
-            .always(always);
-        }.bind(this);
-
-        var success = function(done, data) {
-            var $replySuccess = $('.replySuccess');
-
-            $('.message').val('');
-            $('.name').val('');
-            $('.email').val('');
-            $('.phone').val('');
-            $replySuccess.removeClass('hide');
-            done(data);
-        }.bind(this);
-
-        var trackEvent = function(done, data) {
-            var category = $('.itemCategory').val();
-            var subcategory = $('.itemSubcategory').val();
-
-            this.track({
-                category: 'Reply',
-                action: 'ReplySuccess',
-                custom: ['Reply', category, subcategory, 'ReplySuccess', itemId].join('::')
-            });
-            done();
-        }.bind(this);
-
-        var trackTracking = function(done, data) {
-            var $view = $('#partials-tracking-view');
-            var tracking;
-
-            tracking = $('<div></div>').append(data);
-            tracking = $('#partials-tracking-view', tracking);
-            if (tracking.length) {
-                $view.trigger('updateHtml', tracking.html());
-            }
-            done();
-        }.bind(this);
-
-        var trackGraphite = function(done) {
-            var location = this.app.session.get('location');
-            var platform = this.app.session.get('platform');
-
-            statsd.increment([location.name, 'reply', 'success', platform]);
-            done();
-        }.bind(this);
-
-        var always = function() {
-            $('.spinner').addClass('hide');
-        }.bind(this);
-
-        var fail = function(data) {
-            trackFail();
-        }.bind(this);
-
-        var trackFail = function() {
-            var location = this.app.session.get('location');
-            var platform = this.app.session.get('platform');
-
-            statsd.increment([location.name, 'reply', 'error', platform]);
-        }.bind(this);
-
-        asynquence().or(fail)
-                .then(validate)
-                .then(post)
-                .gate(success, trackEvent, trackTracking, trackGraphite);
-    },
-    validateForm: function(email, name, message) {
-        if((this.isEmpty(email, 'email') || this.isEmpty(name, 'name') || this.isEmpty(message, 'message')) && this.isEmail(email, 'email')) {
-            return true;
-        }
-        return false;
-    },
-    validateField: function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        event.stopImmediatePropagation();
-
-        var field = $(event.currentTarget).attr('name');
-        var value = $(event.currentTarget).val();
-        var result = this.isEmpty(value, field);
-
-        if(field === 'email' && result) {
-            this.isEmail(value, field);
-        }
-    },
-    isEmpty: function (value,field) {
-        if(value === ''){
-            $('span.' + field).text('Por favor complete este campo.').removeClass('hide');
-            $('fieldset.' + field).addClass('error');
-            $('fieldset.' + field + ' span.icons').addClass('icon-attention');
-            return false;
-        }else{
-            $('span.' + field).addClass('hide');
-            $('fieldset.' + field).removeClass('error');
-            $('fieldset.' + field + ' span.icons').removeClass('icon-attention');
-            return true;
-        }
-    },
-    isEmail: function (value,field) {
-        var expression = /^[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,6})$/;
-        if(!expression.test(value)){
-            $('span.' + field).text('La dirección de correo electrónico es inválida.').removeClass('hide');
-            $('fieldset.' + field).addClass('error');
-            $('fieldset.' + field + ' span.icons').addClass('icon-attention');
-            return false;
-        }else{
-            $('span.' + field).addClass('hide');
-            $('fieldset.' + field).removeClass('error');
-            $('fieldset.' + field + ' span.icons').removeClass('icon-attention');
-            return true;
-        }
     }
 });
 
