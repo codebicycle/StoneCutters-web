ee1a949b63a33af9517b53ad367bf32ab388949d
Mauricio Scardaccione <mauricio.scardaccione@olx.com>
2015-01-23 14:40:56 -0300

    Expired and unavailable items

--- app/localized/default/app/views/desktop/items/partials/iteminfo.js
+++ app/localized/default/app/views/desktop/items/partials/iteminfo.js
@@ -1,11 +1,28 @@
 'use strict';
 
 var Base = require('../../../../../../common/app/bases/view');
+var _ = require('underscore');
 
 module.exports = Base.extend({
     className: 'item-info',
     id: 'item-info',
-    tagName: 'ul'
+    tagName: 'ul',
+
+    getTemplateData: function() {
+        var data = Base.prototype.getTemplateData.call(this);
+        var currentYear = new Date().getFullYear();
+        var yearToShow;
+
+        if (data.item.date.year < currentYear) {
+            yearToShow = data.item.date.year;
+        }
+
+
+        return _.extend({}, data, {
+            yearToShow: yearToShow
+        });
+    }
+
 });
 
 module.exports.id = 'items/partials/iteminfo';
