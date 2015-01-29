3dc88a52ab968991f8f72cb6dce1899d00e64940
Juan Baragli <juan.baragli@olx.com>
2015-01-29 15:21:11 -0300

    [mobile-web-ONEWEB-665] myads :: added breadcrumb

--- app/localized/common/app/views/items/delete.js
+++ app/localized/common/app/views/items/delete.js
@@ -2,6 +2,7 @@
 
 var Base = require('../../bases/view');
 var _ = require('underscore');
+var breadcrumb = require('../../../../../modules/breadcrumb');
 
 module.exports = Base.extend({
     className: 'items_delete_view',
@@ -11,8 +12,10 @@ module.exports = Base.extend({
     getTemplateData: function() {
         var data = Base.prototype.getTemplateData.call(this);
 
-        return data;
-    },
+        return _.extend({}, data, {
+            breadcrumb: breadcrumb.get.call(this, data)
+        });
+    }
 });
 
 module.exports.id = 'items/delete';
