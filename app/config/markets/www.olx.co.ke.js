8ff88c08b2efff7f4a2edcc1bcf55ca04858653f
Roberto Braile <roberto.braile@olx.com>
2015-02-06 16:52:34 -0300

    [ONEWEB-720] add a phone number in the posting in nigeria and kenya

--- app/config/markets/www.olx.co.ke.js
+++ app/config/markets/www.olx.co.ke.js
@@ -1,4 +1,12 @@
 module.exports = {
+    post_customer_contact: {
+        phone: '0702-170-824',
+        contact_photo: '/images/desktop/posting_customer/ke.jpg',
+        horary: 'Call us Monday to Friday from 9am to 5pm',
+        name: 'Rahab',
+        work: 'Customer Support Manager',
+        phrase: '"Our team is here to help!"'
+    },
     post_banner: {
         image: 'post-africa'
     },
