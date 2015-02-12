481d58b1151476356185c40fa4e38f3634fb6eee
Guillermo Maquieira <guillermom@olx.com>
2015-02-12 10:43:20 -0300

    [ONEWEB-769] :: Hydra :: Uruguay ON

--- app/config/markets/www.olx.com.uy.js
+++ app/config/markets/www.olx.com.uy.js
@@ -41,9 +41,6 @@ module.exports = {
     },
     tracking: {
         trackers: {
-            hydra: {
-                enabled: false
-            },
             tagmanager: {
                 enabled: true
             }
