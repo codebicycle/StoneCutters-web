f76f26e98e97ec4dc00dd6b60e9f3452260a2487
OLX - Melisa Bok <melisa.bok@olx.com>
2015-02-25 17:08:12 -0300

    add sixpack to search

---
+++ app/localized/default/app/views/html4/searches/search.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var Base = require('../../../../../common/app/bases/view').requireView('searches/search');
+var helpers = require('../../../../../../helpers');
+var _ = require('underscore');
+var Sixpack = require('../../../../../../../shared/sixpack');
+
+module.exports = Base.extend({
+    className: 'items_search_view',
+    getTemplateData: function() {
+        var data = Base.prototype.getTemplateData.call(this);
+        var sixpack = new Sixpack({
+            clientId: this.app.session.get('sixpackId'),
+            platform: this.app.session.get('platform'),
+            market: this.app.session.get('location').abbreviation,
+            experiments: this.app.session.get('experiments')
+        });
+        var experiment = sixpack.experiments.html4ShowShops;
+        console.log('search viewhtml4', experiment);
+ 
+        return _.extend({}, data, {
+            experiment: experiment.alternative
+        });
+    }
+});
+
+module.exports.id = 'searches/search';
