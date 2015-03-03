1c1c8cd065424dc71356b9698123c47b5d4e32c1
cristianele <cristian.elena@olx.com>
2015-03-03 12:32:02 -0300

    Add params to redirect

--- app/controllers/items.js
+++ app/controllers/items.js
@@ -569,7 +569,9 @@ function reply(params, callback) {
 
                 done.abort();
                 return helpers.common.redirect.call(this, '/myolx/conversation/' + threadId, null, {
-                    status: 302
+                    status: 302,
+                    replace: true,
+                    trigger: true
                 });
             }
          }
