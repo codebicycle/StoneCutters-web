a7a0064ffa885fad016c423cd7534e4851547b12
Nicolas Molina <nicolas.molina@olx.com>
2015-03-03 14:47:51 -0300

    Tracking :: Interstitial page view to event

---
+++ app/localized/default/app/views/html5/pages/interstitial.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var _ = require('underscore');
+var Base = require('../../../../../common/app/bases/view').requireView('pages/interstitial');
+var helpers = require('../../../../../../helpers');
+var config = require('../../../../../../../shared/config');
+var maxAge = config.get(['interstitial', 'time'], 60000);
+
+module.exports = Base.extend({
+    postRender: function() {
+        var $view = $('#partials-tracking-view');
+        var interstitial = this.$('#interstitial');
+        var header = $('.header_index_view');
+        var views = $('[data-view]');
+
+        if (!interstitial.length) {
+            return;
+        }
+
+        header.before(interstitial);
+        $(views).addClass('hide');
+
+        $('.downloadApp').on('click', function callback(event) {
+            this.app.session.persist({
+                downloadApp: '1'
+            });
+        }.bind(this));
+
+        $('.closeInterstitial').on('click', function callback(event) {
+            $(views).removeClass('hide');
+            interstitial.remove();
+            this.app.session.clear('interstitial');
+        }.bind(this));
+
+        this.attachTrackMe();
+        this.track({
+            category: 'Interstitial',
+            action: 'View',
+            custom: 'Interstitial::-::-::View'
+        });
+        this.app.session.persist({
+            showInterstitial: '1'
+        }, {
+            maxAge: maxAge
+        });
+    }
+});
