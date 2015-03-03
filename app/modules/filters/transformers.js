8d097d171bec655cf37556379b0430b3556db73f
Nicolas Molina <nicolas.molina@olx.com>
2015-03-03 12:19:56 -0300

    Filters :: Neigborhood locations sort

--- app/modules/filters/transformers.js
+++ app/modules/filters/transformers.js
@@ -6,6 +6,7 @@ var translations = require('../../../shared/translations');
 module.exports = {
     city: city,
     state: state,
+    neighborhood: neighborhood,
     kilometers: kilometers,
     bathrooms: bathrooms,
     bedrooms: bedrooms,
@@ -24,14 +25,14 @@ function buildKilometerRange(to, label, dictionary) {
     };
 }
 
-function sortFilterCityAndState(locations, options) {
+function sortFilterLocations(locations, options, count) {
     var platform = options.app.session.get('platform');
     var hiddenLocations;
 
-    if (platform !== 'desktop' || !locations || !locations.length || locations.length <= 12) {
+    if (platform !== 'desktop' || !locations || !locations.length || locations.length <= count) {
         return locations;
     }
-    hiddenLocations = locations.slice(12).sort(function sortByName(location1, location2) {
+    hiddenLocations = locations.slice(count).sort(function sortByName(location1, location2) {
         if (location1.value > location2.value) {
             return 1;
         }
@@ -40,12 +41,12 @@ function sortFilterCityAndState(locations, options) {
         }
         return 0;
     });
-    return locations.slice(0, 12).concat(hiddenLocations);
+    return locations.slice(0, count).concat(hiddenLocations);
 }
 
 function city(filter, options) {
     filter.set({
-        value: sortFilterCityAndState(filter.get('value'), options)
+        value: sortFilterLocations(filter.get('value'), options, 12)
     }, {
         unset: false
     });
@@ -54,7 +55,16 @@ function city(filter, options) {
 
 function state(filter, options) {
     filter.set({
-        value: sortFilterCityAndState(filter.get('value'), options)
+        value: sortFilterLocations(filter.get('value'), options, 12)
+    }, {
+        unset: false
+    });
+    return filter;
+}
+
+function neighborhood(filter, options) {
+    filter.set({
+        value: sortFilterLocations(filter.get('value'), options, 8)
     }, {
         unset: false
     });
