4035950b64894e1d62c14b121f1b1518504b835b
Andres Risso <andresr@olx.com>
2015-03-09 14:34:09 -0300

    [ONEWEB-899] BiDi Support :: Remove Iris localization
    Added localized version of counter in category model

--- app/models/category.js
+++ app/models/category.js
@@ -1,14 +1,20 @@
 'use strict';
 
 var Base = require('../bases/model');
+var config = require('../../shared/config');
+var helpers = require('../helpers');
 
 module.exports = Base.extend({
     url: '/countries/:location/categories/:id',
     idAttribute: 'id',
     parse: function(result) {
+        var digits = config.getForMarket(this.app.session.get('location').url, ['layoutOptions', 'digits'], {});
         var Collection = require('../collections/categories');
-
-        result.children = new Collection(result.children);
+        
+        result.children = new Collection(result.children, {parse: true, app: this.app});
+        result.localized = {
+            'counter': (digits !== 'western-arabic') ? helpers.numbers.translate(result.counter, {to: digits}) : result.counter
+        };
         return result;
     },
     checkSlug: function(categorySlug, urlSlug) {
