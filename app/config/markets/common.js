1e9782b52609f2428e8e5243a148fe0354cf4306
Nicolas Molina <nicolas.molina@olx.com>
2015-03-19 17:55:27 -0300

    Merge

--- app/config/markets/common.js
+++ app/config/markets/common.js
@@ -170,7 +170,7 @@ module.exports = {
             },
             ninja: {
                 enabled: true,
-                platforms: ['wap', 'html4', 'html5', 'desktop'],
+                platforms: ['desktop'],
                 noscript: {
                     platforms: ['wap', 'html4', 'html5', 'desktop']
                 },
