9699b155c0e6198eaa7a46cc2055fd2d3ff24f2f
Nicolas Molina <nicolas.molina@olx.com>
2015-03-26 13:21:21 -0300

    [DGD-1] Team Dashboard :: More metrics

--- app/localized/default/app/views/desktop/header/index.js
+++ app/localized/default/app/views/desktop/header/index.js
@@ -1,12 +1,16 @@
 'use strict';
 
-var Base = require('../../../../../common/app/bases/view').requireView('header/index');
 var _ = require('underscore');
+var Base = require('../../../../../common/app/bases/view').requireView('header/index');
+var statsd = require('../../../../../../../shared/statsd')();
 
 module.exports = Base.extend({
     tagName: 'header',
     id: 'header-view',
     className: 'header-view',
+    events: {
+        'click [data-dgd-track]': 'onClickDgdTrack'
+    },
     postRender: function () {
         this.app.router.appView.on('posting:start', this.onPostingStart.bind(this));
         this.app.router.appView.on('posting:end', this.onPostingEnd.bind(this));
@@ -18,5 +22,11 @@ module.exports = Base.extend({
     onPostingEnd: function () {
         $('.posting, .search-form').removeClass('disabled');
         $('.posting-title').addClass('disabled');
+    },
+    onClickDgdTrack: function(event) {
+        var $elem = $(event.currentTarget);
+        var type = $elem.data('dgd-track');
+
+        statsd.increment([this.app.session.get('location').abbreviation, 'dgd', 'home', type, this.app.session.get('platform')]);
     }
 });
