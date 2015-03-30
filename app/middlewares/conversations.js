8a3dc7b129410ee194dae72f42f0c18f8cc56226
Mauricio Scardaccione <mauricio.scardaccione@olx.com>
2015-03-30 17:46:17 -0300

    Test

--- app/middlewares/conversations.js
+++ app/middlewares/conversations.js
@@ -4,8 +4,20 @@ var helpers = require('../helpers');
 
 module.exports = function(params, next) {
     var user = this.app.session.get('user');
+    var messages = this.app.session.get('messages') || false;
+    var query = {};
+    /*this.app.session.persist({
+        messages: '4eswsGi5zgOHlSYD72wpf101fcyIvCWhZ2Kd21hXMmxfYrRXk2KWkpZrXfvov3emgzXUWiy%2F45V%2Bc5yG8OoM8uTTteQFxkj%2Bw8%2Bzf4jfKxqINOFH%2B2ERvqpev%2BMlU6YBegCB6uRHhq0ikj%2Bkogsb%2FFK8r0IH0YJ65JBU4Z0IhggRMx47V2UvXKUp%2FT3i4iqQ'
+    });*/
 
-    if (!user) {
+    if (!user && messages) {
+        query = {
+            hash: 'hash',
+            location: this.app.session.get('location').url,
+            platform: this.app.session.get('platform')
+        }
+        console.log(messages);
+        console.log(query);
         return next();
     }
 
@@ -31,3 +43,5 @@ module.exports = function(params, next) {
         next();
     }
 };
+
+
