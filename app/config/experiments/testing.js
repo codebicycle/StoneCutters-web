cea5a564bf73a82308ae8c34c3bc6645ce53e428
Santiago Hollmann <santiago.hollmann@gmail.com>
2015-04-01 17:46:45 -0300

    Removed space between two countries

--- app/config/experiments/testing.js
+++ app/config/experiments/testing.js
@@ -61,7 +61,7 @@ module.exports = {
     desktopDGD23ShowSimplifiedReplyForm: {
         enabled: true,
         platforms: ['desktop'],
-        markets: ['ar', 'pe', 'co', 've', 'uy', 'ni', 'gt', 'za', 'ke', 'ng', 'ug', 'py', 'ec', 'bo', 'sn', 'es', 'cm', 'tz', 'hn', 'cr',                       'pa', 'gh', 'sv'],
+        markets: ['ar', 'pe', 'co', 've', 'uy', 'ni', 'gt', 'za', 'ke', 'ng', 'ug', 'py', 'ec', 'bo', 'sn', 'es', 'cm', 'tz', 'hn', 'cr', 'pa', 'gh', 'sv'],
         name: 'desktop-dgd23-show-simplified-reply-form',
         alternatives: {
             CONTROL: 'control',
