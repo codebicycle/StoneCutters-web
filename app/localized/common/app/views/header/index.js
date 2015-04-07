cdd2b19d2feb966aa64be1386860f78c8b5b0fc4
Mauricio Scardaccione <mauricio.scardaccione@olx.com>
2015-04-07 10:27:41 -0300

    notifications logout º

--- app/localized/common/app/views/header/index.js
+++ app/localized/common/app/views/header/index.js
@@ -16,6 +16,7 @@ module.exports = Base.extend({
         var banner = config.get(['migration', location.url, 'banner'], false);
         var languageAbbreviation = this.app.session.get('languages')._byId[this.app.session.get('selectedLanguage')].isocode.toLowerCase();
         var currentRoute = this.app.session.get('currentRoute');
+        var messages = this.app.session.get('messages');
         var postLinkUrl = false;
 
         if (location.url === 'www.olx.com.mx') {
@@ -32,7 +33,8 @@ module.exports = Base.extend({
             banner: banner ? data.template + '/partials/migration/banner-' + location.abbreviation.toLowerCase() + '.html' : false,
             languageAbbreviation: languageAbbreviation,
             isPostFlow: currentRoute.controller === 'post',
-            postLinkUrl: postLinkUrl
+            postLinkUrl: postLinkUrl,
+            messages: messages
         });
     },
     getPostLink: function() {
