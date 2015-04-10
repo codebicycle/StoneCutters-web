ead57895277ba63805af7c6d3b068d8f5045ac28
Mauricio Scardaccione <mauricio.scardaccione@olx.com>
2015-04-10 14:52:36 -0300

    Fix send messageFix send messagess

--- app/localized/default/app/views/desktop/users/conversation.js
+++ app/localized/default/app/views/desktop/users/conversation.js
@@ -5,6 +5,7 @@ var async = require('async');
 var asynquence = require('asynquence');
 var Base = require('../../../../../common/app/bases/view').requireView('users/conversation');
 var Conversation = require('../../../../../../models/conversation');
+var translations = require('../../../../../../../shared/translations');
 var helpers = require('../../../../../../helpers');
 
 module.exports = Base.extend({
@@ -21,6 +22,7 @@ module.exports = Base.extend({
         return data;
     },
     postRender: function() {
+        this.dictionary = translations.get(this.app.session.get('selectedLanguage'));
         var $conversation = this.$('ul.conversation');
 
         this.scrollBottom($conversation);
@@ -47,8 +49,7 @@ module.exports = Base.extend({
             .then(validate.bind(this))
             .then(submit.bind(this))
             .then(prepare.bind(this))
-            .then(success.bind(this));
-            //.val(change.bind(this));
+            .val(success.bind(this));
 
         function validate(done) {
             if (!this.validate(this.$('[data-messageText]'))) {
@@ -80,29 +81,25 @@ module.exports = Base.extend({
 
         function success(done) {
             var $conversation = this.$('ul.conversation');
-            var newMessage = '<li class="conversation-chat"><span class="name">Vos</span><span class="date"> - <time> Recien </time></span><p class="message">' + this.parentView.getConversation().get('message') + '</p></li>';
+            var date = new Date();
+            var newMessage;
+
+            date = {
+                year: date.getFullYear(),
+                month: date.getMonth() + 1,
+                day: date.getDate(),
+                hour: date.getHours(),
+                minute: date.getMinutes(),
+                second: date.getSeconds()
+            };
+            date = helpers.timeAgo(date);
+            date = this.dictionary[date.dictionary] + ' ' + date.hour;
+            newMessage = '<li class="conversation-chat"><span class="name">' + this.dictionary["myolx.You"] + '</span><span class="date"> - <time> ' + date + ' </time></span><p class="message">' + this.parentView.getConversation().get('message') + '</p></li>';
             $conversation.append(newMessage);
             this.scrollBottom($conversation);
             this.$('.spinner').addClass('display-none');
             this.$('.btn.orange').removeClass('display-none');
             this.$('[data-messageText]').val('');
-            /*this.app.fetch({
-                conversation: {
-                    model: 'Conversation',
-                    params: params
-                }
-            }, {
-                readFromCache: false
-            }, done.errfcb);*/
-        }
-
-        function change(res) {
-            this.parentView.conversation = res.conversation;
-            this.parentView.getConversation().set('user', this.app.session.get('user'));
-            this.parentView.getConversation().set('platform', this.app.session.get('platform'));
-            this.parentView.getConversation().set('location', this.app.session.get('location').url);
-            this.parentView.getConversation().set('country', this.app.session.get('location').abbreviation);
-            //this.render();
         }
 
         function fail(err) {
