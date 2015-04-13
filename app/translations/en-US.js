02ec15e431222f27df6578ea716e8aa12860471d
Lautaro Dragan <lautaro.dragan@olx.com>
2015-04-13 11:24:37 -0300

    Removing translation entries added manually...

--- app/translations/en-US.js
+++ app/translations/en-US.js
@@ -243,8 +243,6 @@ module.exports = {
     'item.Ended_New': 'Ended',
     'item.Rejected': 'Rejected',
     'item.RemoveFromFavorites': 'Remove from favorites',
-    'item.FlagAsSpamOrScam': 'Flag ad',
-    'item.FlagAsSpamOrScamDone': 'Ad flagged',
     'item.SeeMoreAds': 'See more ads in <<LINK_TO_CATEGORY>>',
     'item.SelectA_Neighborhood': 'Select a neighborhood',
     'item.sex': 'Sex',
