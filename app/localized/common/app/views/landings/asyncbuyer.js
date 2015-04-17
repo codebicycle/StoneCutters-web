c7becd8a9501e5e8eab99828d818ae35c28bb7ac
Juan Baragli <juan.baragli@olx.com>
2015-04-17 14:33:45 -0300

    removed getTemplateData

--- app/localized/common/app/views/landings/asyncbuyer.js
+++ app/localized/common/app/views/landings/asyncbuyer.js
@@ -4,12 +4,7 @@ var Base = require('../../bases/view');
 var _ = require('underscore');
 
 module.exports = Base.extend({
-    className: 'landings_asyncbuyer_view',
-    getTemplateData: function() {
-        var data = Base.prototype.getTemplateData.call(this);
-
-        return _.extend({}, data, {});
-    }
+    className: 'landings_asyncbuyer_view'
 });
 
 module.exports.id = 'landings/asyncbuyer';
