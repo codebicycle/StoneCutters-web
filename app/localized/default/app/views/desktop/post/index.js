1ad532339a1e7f3315ea5f9d238a5d6e317baa37
cristianele <cristian.elena@olx.com>
2015-04-20 13:58:21 -0300

    ABtest - Add participate

--- app/localized/default/app/views/desktop/post/index.js
+++ app/localized/default/app/views/desktop/post/index.js
@@ -65,7 +65,7 @@ module.exports = Base.extend({
         var customerContact = config.getForMarket(location.url, ['post_customer_contact'], '');
         var sixpackCurrentAlternative = this.app.sixpack.experiments.growthCategorySuggestion ? this.app.sixpack.experiments.growthCategorySuggestion.alternative : '';
 
-        this.app.sixpack.convert(this.app.sixpack.experiments.growthCategorySuggestion, 'starters');
+        this.app.sixpack.participate(this.app.sixpack.experiments.growthCategorySuggestion);
 
         return _.extend({}, data, {
             item: this.getItem(data.item),
