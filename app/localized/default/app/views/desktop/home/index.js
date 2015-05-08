9c95c90e15a340a4c286b253a034f6f74b497081
Andres Risso <andresr@olx.com>
2015-05-08 16:06:05 -0300

    [DGD-109] Create HomePage :: Google

--- app/localized/default/app/views/desktop/home/index.js
+++ app/localized/default/app/views/desktop/home/index.js
@@ -2,9 +2,25 @@
 
 var _ = require('underscore');
 var Base = require('../../../../../common/app/bases/view').requireView('home/index');
+var helpers = require('../../../../../../helpers');
+var Metric = require('../../../../../../modules/metric');
 
 module.exports = Base.extend({
     tagName: 'main',
     id: 'home_view',
-    className: 'home_view'
+    className: 'home_view',
+	events: {
+        'submit .search-form': 'onSearchSubmit'
+    },
+    onSearchSubmit: function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+
+        var query = this.$('.search-form').find('.search-term').val();
+        
+        helpers.common.redirect.call(this.app.router, query ? ('/nf/search/' + query) : '/nf/all-results', null, {
+            status: 200
+        });
+    }
 });
