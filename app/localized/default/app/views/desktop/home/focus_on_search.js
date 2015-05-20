6bf667d6a189275e5539f29b84778a351ef1d779
Nicolas Molina <nicolas.molina@olx.com>
2015-05-20 12:29:16 -0300

    [DGD-117] Homepage Experiment ZA & CO :: Create Homepage Amazon :: Search location

--- app/localized/default/app/views/desktop/home/focus_on_search.js
+++ app/localized/default/app/views/desktop/home/focus_on_search.js
@@ -3,7 +3,6 @@
 var _ = require('underscore');
 var Base = require('../../../../../common/app/bases/view').requireView('home/focus_on_search');
 var helpers = require('../../../../../../helpers');
-var Metric = require('../../../../../../modules/metric');
 var utils = require('../../../../../../../shared/utils');
 
 module.exports = Base.extend({
@@ -11,7 +10,12 @@ module.exports = Base.extend({
     id: 'home_view',
     className: 'home_view',
 	events: {
-        'submit .search-form': 'onSearchSubmit'
+        'showLocalize': 'onShowLocalize',
+        'hideLocalize': 'onHideLocalize',
+        'submit .search-form': 'onSearchSubmit',
+        'click a[data-location]': 'onClickLocation',
+        'click .search-location': 'onShowLocalize',
+        'click .states-list li a': 'onClickState'
     },
     preRender: function() {
         if (!utils.isServer) {
@@ -30,15 +34,48 @@ module.exports = Base.extend({
         this.app.trigger('header:restore');
         this.app.trigger('footer:show');
     },
+    onShowLocalize: function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+
+        this.$('.states-list').fadeIn();
+    },
+    onHideLocalize: function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+
+        this.$('.states-list').fadeOut();
+    },
     onSearchSubmit: function(event) {
         event.preventDefault();
         event.stopPropagation();
         event.stopImmediatePropagation();
 
-        var query = this.$('.search-form').find('.search-term').val();
+        var search = this.$('.search-form').find('.search-term').val();
+        var url = search ? ('/nf/search/' + search) : '/nf/all-results';
         
-        helpers.common.redirect.call(this.app.router, query ? ('/nf/search/' + query) : '/nf/all-results', null, {
+        helpers.common.redirect.call(this.app.router, utils.fullizeUrl(url, this.app), null, {
+            pushState: false,
             status: 200
         });
+    },
+    onClickLocation: function(event) {
+        this.app.session.persist({
+            siteLocation: $(event.currentTarget).data('location')
+        });
+    },
+    onClickState: function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+
+        var $location = this.$('.search-location');
+        var $state = $(event.currentTarget);
+
+        $location.data('location', $state.data('location'));
+        $location.text($state.text());
+        this.$el.trigger('hideLocalize');
     }
 });
